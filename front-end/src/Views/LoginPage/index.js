63bfd3a2ef5d1e2c425b5bbf6d04d9431bd11d24
Adrian Mostowski <adaxelx@o2.pl>
2019-11-21 18:22:16 +0100

    add links (new pass/acc) and social-media boxes

--- front-end/src/Views/LoginPage/index.js
+++ front-end/src/Views/LoginPage/index.js
@@ -2,16 +2,18 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
 
 import "Views/LoginPage/style.css";
 
 class LoginPage extends React.Component {
   state = {
-    email: "",
-    password: "",
-    validated: false
+    email: "", // przechowywanie maila
+    password: "", // przechowywanie hasła
+    validated: false // przechowywanie stanu czy ktoś juz kliknął przycisk walidacji czy nie
   };
 
+  // Funkcja przechowująca aktualne wartości wpisanego maila,hasła
   onChange = e => {
     const type = e.target.type;
     const value = e.target.value;
@@ -20,26 +22,46 @@ class LoginPage extends React.Component {
     });
   };
 
+  // Funkcja podsumowująca formularz
   handleSubmit = event => {
-    const form = event.currentTarget;
-    event.preventDefault();
+    const form = event.currentTarget; // formularz
+    const { password, email } = this.state; // destrukturyzacja stanu,maila
+    event.preventDefault(); // zapobiega odświeżaniu strony
+
+    //Sprawdzenie czy formularz został poprawnie uzupełniony
     if (form.checkValidity() === false) {
       event.preventDefault();
-      event.stopPropagation();
+      event.stopPropagation(); // zatrzymuje event
+    } else {
+      console.log(password, email); // jezeli wszystko okej wyswietla stan(w przyszlosci bedzie przekazywany do backendu)
     }
 
+    // przycisk został kliknięty więc zmieniamy stan
     this.setState({
       validated: true
     });
   };
 
   render() {
-    const { email, password, validated } = this.state;
-    const { onChange, handleSubmit } = this;
+    const { email, password, validated } = this.state; // destrukturyzacja stanu,emaila
+    const { onChange, handleSubmit } = this; // destrukturyzacja funkcji
     return (
-      <Container className="container">
+      <Container className="loginPage">
         <h1 className="display-4">Logowanie</h1>
-        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <section className="loginPage__media">
+          <button className="media__button media__button--facebook">
+            Facebook
+          </button>
+          <button className="media__button media__button--google">
+            Google
+          </button>
+        </section>
+        <Form
+          noValidate
+          validated={validated}
+          onSubmit={handleSubmit}
+          className="loginPage__form primary"
+        >
           <Form.Group controlId="formGroupEmail">
             <Form.Label>Email:</Form.Label>
             <Form.Control
@@ -48,6 +70,7 @@ class LoginPage extends React.Component {
               required
               value={email}
               onChange={onChange}
+              className="loginPage__input"
             />
             <Form.Control.Feedback type="invalid">
               Podaj właściwy email
@@ -63,13 +86,22 @@ class LoginPage extends React.Component {
               value={password}
               onChange={onChange}
               required
+              minLength="6"
             />
             <Form.Control.Feedback type="invalid">
               Podaj właściwe hasło
             </Form.Control.Feedback>
           </Form.Group>
-          <Button type="submit">Submit form</Button>
+          <Button type="submit">Zaloguj</Button>
         </Form>
+        <section className="loginPage__links">
+          <Link to="/newPassword" className="loginPage__link">
+            Zapomniałeś hasła?
+          </Link>
+          <Link to="/newAccount" className="loginPage__link">
+            Utwórz konto!
+          </Link>
+        </section>
       </Container>
     );
   }
