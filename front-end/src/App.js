a83023e3b3656d488ac7bdef6122154aeabe626e
Damian <7dami77@gmail.com>
2019-11-30 20:26:06 +0100

    Added UserProfile page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
 import "./App.css";
+import UserProfilePage from "views/UserProfilePage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route path="/user" exact component={UserProfilePage} />
+          </Switch>
+        </Router>
+      </div>
+    );
   }
 }
 
