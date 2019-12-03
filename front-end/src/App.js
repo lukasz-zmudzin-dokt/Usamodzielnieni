761da85851005b453a3365baa1f65d5120c41830
Salabim-mb <48840472+Salabim-mb@users.noreply.github.com>
2019-12-03 12:55:23 +0100

    working navbar, to be connected to api?

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,15 +3,16 @@ import "./App.css";
 import UserProfilePage from "Views/UserProfilePage";
 import LoginPage from "Views/LoginPage";
 import RegisterPage from "Views/RegisterPage";
-import HeaderTemplate from "Views/PageHeader/headerTemplate";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HeaderTemplate from "./Views/PageHeader/headerTemplate";
 
 class App extends React.Component {
-  render() {
+  render() { //HeaderTemplate start= obecna ścieżka, isLoggedIn = status do API
     return (
       <div>
+        <HeaderTemplate start={"/."} isLoggedIn={true}/>
+        <br/><br/><br/>
         <Router>
-          <HeaderTemplate />
           <Switch>
             <Route path="/user" exact component={UserProfilePage} />
             <Route path="/login" exact component={LoginPage} />
--- front-end/src/Views/PageHeader/headerTemplate.js
+++ front-end/src/Views/PageHeader/headerTemplate.js
@@ -1,40 +1,73 @@
 import React from 'react';
 import {Navbar, Nav, Container, Button, Form} from "react-bootstrap";
 
-import { Link } from "react-router-dom";
 import 'Views/PageHeader/headerLayout.css';
 import logo from '../graphics/logo.png';
 
 class HeaderTemplate extends React.Component {
 
-    handleSelect = event => {
+    constructor(props){
+        super(props);
+        this.state = {
+            currentLocation: this.props.start,
+            isLoggedIn: this.props.isLoggedIn //status pobierany jakimś getem do api?
+        };
+    }
 
+    displayMenu() {
+        if (this.state.currentLocation !== "/")
+            return (
+                <Nav pullCenter className="mr-auto">
+                    <Nav.Link id="cvEditor" href="/cvEditor">Kreator CV</Nav.Link>
+                    <Nav.Link id="learningTheRopes">Od czego zacząć?</Nav.Link>
+                    <Nav.Link id="jobOffers">Oferty pracy</Nav.Link>
+                    <Nav.Link id="jobDescriptions">Opis stanowisk</Nav.Link>
+                    <Nav.Link id="personalityTests">Testy</Nav.Link>
+                    <Nav.Link id="stories">Historie usamodzielnionych</Nav.Link>
+                    <Nav.Link id="moneyMgmt">Zarządzanie budżetem</Nav.Link>
+                </Nav>
+            );
+    };
+
+    displayButtonSet() {
+        if (this.state.isLoggedIn)
+            return (
+                <Form inline pull-right>
+                    <Button id="userProfile" variant="light" href="/user">Profil</Button>
+                    <Button id="loginButton" variant="outline-light" onClick={e => this.userLogout(e)}>Wyloguj</Button>
+                </Form>
+            );
+        else
+            return (
+                <Form inline pull-right>
+                    <Button id="userProfile" variant="light" href="/newAccount">Rejestracja</Button>
+                    <Button id="loginButton" variant="outline-light" href="/login">Logowanie</Button>
+                </Form>
+            );
+    };
+
+    userLogout = e => {
+        this.setState({
+            isLoggedIn: false
+        });
+        e.preventDefault();
+        window.location.replace("/");
     };
 
     render() {
-        const handleSelect = this;
         return(
             <Container className = "LayoutTemplate">
                 <Navbar bg="dark" variant="dark" fixed="top" expand="lg">
                     <Navbar.Brand id="navbar_logo">
-                        <img id="charity_logo" width='200vh' src={logo} alt="Usamodzielnieni"/>
+                        <a href="/"><img id="charity_logo" width='200vh' src={logo} alt="Usamodzielnieni"/></a>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="topMenu"/>
                     <Navbar.Collapse id="topMenu">
-                        <Nav className="mr-auto" onSelect={handleSelect}>
-                            <Nav.Link id="cvCreator">Kreator CV</Nav.Link>
-                            <Nav.Link id="learningTheRopes">Od czego zacząć?</Nav.Link>
-                            <Nav.Link id="jobOffers">Oferty pracy</Nav.Link>
-                            <Nav.Link id="jobDescriptions">Opis stanowisk</Nav.Link>
-                            <Nav.Link id="personalityTests">Testy</Nav.Link>
-                            <Nav.Link id="stories">Historie usamodzielnionych</Nav.Link>
-                            <Nav.Link id="moneyMgmt">Zarządzanie budżetem</Nav.Link>
-                        </Nav>
-                        <Form inline>
-                            <Nav.Link id="phones" variant="light"><Link to="/user">Telefony</Link></Nav.Link>
-                            <Button id="loginButton" variant="outline-light"><Link to="/login">Logowanie</Link></Button>
-                        </Form>
+                        {this.displayMenu()}
                     </Navbar.Collapse>
+                    <div id="nav_buttons">
+                        {this.displayButtonSet()}
+                    </div>
                 </Navbar>
             </Container>
         );
