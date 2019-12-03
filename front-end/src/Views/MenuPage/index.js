4f4a26a5c604601832633c748c2a5edb97f0b320
Miko1012 <mczarkowski7@outlook.com>
2019-12-03 14:56:20 +0100

    upload old files

---
+++ front-end/src/Views/MenuPage/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
+
+import "Views/LoginPage/style.css";
+
+class LoginPage extends React.Component {
+
+
+
+    render() {
+        return (
+
+            <Container className="menuPage">
+
+                <div class="background-img">
+                    <h1> hej hej </h1>
+                </div>
+
+            </Container>
+
+        );
+    }
+}
+
+export default LoginPage;
