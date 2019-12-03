88047f119f752dc677cd0e53392680ffef969d33
Miko1012 <mczarkowski7@outlook.com>
2019-12-03 20:19:52 +0100

    upload old files

--- front-end/src/Views/MenuPage/index.js
+++ front-end/src/Views/MenuPage/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import { Link } from "react-router-dom";
 
-import "Views/LoginPage/style.css";
+import FbIcon from "./facebook-128.ico"
+import LinkedInIcon from "./linkedin-128.ico"
+import YouTubeIcon from "./youtube-128.ico"
+
+import "Views/MenuPage/style.css";
 
 class LoginPage extends React.Component {
 
@@ -14,11 +16,32 @@ class LoginPage extends React.Component {
 
             <Container className="menuPage">
 
+
+
                 <div class="background-img">
                     <h1> hej hej </h1>
+                    <img src="./Views/MenuPage/background_menu.tif" alt="" />
+                </div>
+
+                <div class="footer">
+                    <div class="row justify-content-end mt-4">
+                        <div class="col-8 mr-5">
+                            <h2>KONTAKT</h2>
+                        </div>
+                    </div>
+                    <div class="row justify-content-end mt-3">
+                        <div class="col-8 align-self-end mr-5 pr-5">
+                            <img src={FbIcon} alt="" class="footer-icon"/>
+                            <img src={LinkedInIcon} alt="" class="footer-icon"/>
+                            <img src={YouTubeIcon} alt="" className="footer-icon"/>
+                        </div>
+                    </div>
+
                 </div>
 
             </Container>
+
+
 
         );
     }
