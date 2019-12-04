2c4a46271580c5bc9375a2448686da0fe93e1875
Salabim-mb <48840472+Salabim-mb@users.noreply.github.com>
2019-12-04 20:57:17 +0100

    poprawa generowania headera

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,15 +5,17 @@ import LoginPage from "Views/LoginPage";
 import RegisterPage from "Views/RegisterPage";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HeaderTemplate from "./Views/PageHeader/headerTemplate";
+import CVEditorPage from "./Views/CVEditorPage";
 
 class App extends React.Component {
-  render() { //HeaderTemplate start= obecna ścieżka, isLoggedIn = status do API
+  render() {
     return (
       <div>
-        <HeaderTemplate start={"/."} isLoggedIn={true}/>
-        <br/><br/><br/>
+          {document.title = "Usamodzielnieni"}
+          <HeaderTemplate/>
         <Router>
           <Switch>
+            <Route path="/cvEditor" exact component={CVEditorPage} />
             <Route path="/user" exact component={UserProfilePage} />
             <Route path="/login" exact component={LoginPage} />
             <Route path="/newAccount" exact component={RegisterPage} />
