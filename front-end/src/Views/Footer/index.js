fa31a9b392c08463d10f8c07e0576876774eb523
Miko1012 <mczarkowski7@outlook.com>
2019-12-04 21:39:01 +0100

    białe kafelki żeby wyróżnić miejsce do kliknięcia

--- front-end/src/Views/Footer/index.js
+++ front-end/src/Views/Footer/index.js
@@ -1,32 +1,34 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 
-import FbIcon from "../../img/facebook-128.ico"
-import LinkedInIcon from "../../img/linkedin-128.ico"
-import YouTubeIcon from "../../img/youtube-128.ico"
+import FbIcon from "../../assets/facebook-128.ico";
+import LinkedInIcon from "../../assets/linkedin-128.ico";
+import YouTubeIcon from "../../assets/youtube-128.ico";
+import Background from "../../assets/menu-backdrop.png";
 
 import "Views/Footer/style.css";
 
 class Footer extends React.Component {
 
     render() {
+        console.log(Background);
         return (
 
             <Container className="Footer">
 
-                <div class="footer">
-                    <div class="row justify-content-end mt-4">
-                        <div class="col-8 mr-5">
+                <div className="footer">
+                    <div className="row justify-content-end mt-4">
+                        <div className="col-8 mr-5">
                             <h2>KONTAKT</h2>
                         </div>
                     </div>
-                    <div class="row justify-content-end mt-3">
-                        <div class="col-8 align-self-end mr-5 pr-5">
+                    <div className="row justify-content-end mt-3">
+                        <div className="col-8 align-self-end mr-5 pr-5">
                             <a href="https://www.facebook.com/fundacjaoneday/">
-                                <img src={FbIcon} alt="" class="footer-icon"/>
+                                <img src={FbIcon} alt="" className="footer-icon"/>
                             </a>
                             <a href="https://www.linkedin.com/company/fundacja-one-day/about/">
-                                <img src={LinkedInIcon} alt="" class="footer-icon"/>
+                                <img src={LinkedInIcon} alt="" className="footer-icon"/>
                             </a>
                             <a href="https://www.youtube.com/channel/UCvWPmhiC5Q3gYVR3MPilHbQ">
                                 <img src={YouTubeIcon} alt="" className="footer-icon"/>
@@ -36,6 +38,7 @@ class Footer extends React.Component {
                 </div>
 
             </Container>
+
         );
     }
 }
