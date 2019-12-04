ac5d9a1a3a66d15da848f5cb99adbb98289acb00
Salabim-mb <48840472+Salabim-mb@users.noreply.github.com>
2019-12-04 19:41:45 +0100

    Dodanie brakujących plików json i app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,27 @@
 import React from "react";
 import "./App.css";
+import UserProfilePage from "Views/UserProfilePage";
+import LoginPage from "Views/LoginPage";
+import RegisterPage from "Views/RegisterPage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HeaderTemplate from "./Views/PageHeader/headerTemplate";
 import CVEditorPage from "./Views/CVEditorPage";
-import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
-        <div>
-            <Router>
-                <Switch>
-                    <Route path="/" exact component={CVEditorPage} />
-                </Switch>
-            </Router>
-        </div>
+      <div>
+          {document.title = "Usamodzielnieni"}
+          <HeaderTemplate/>
+        <Router>
+          <Switch>
+            <Route path="/cvEditor" exact component={CVEditorPage} />
+            <Route path="/user" exact component={UserProfilePage} />
+            <Route path="/login" exact component={LoginPage} />
+            <Route path="/newAccount" exact component={RegisterPage} />
+          </Switch>
+        </Router>
+      </div>
     );
   }
 }
