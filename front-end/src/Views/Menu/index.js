0d952ffc1a67f2a698f60fff2048002846fa5c57
Miko1012 <mczarkowski7@outlook.com>
2019-12-12 14:08:47 +0100

    wyszarzenie nieaktywnych przycisków

--- front-end/src/Views/Menu/index.js
+++ front-end/src/Views/Menu/index.js
@@ -39,31 +39,14 @@ class Menu extends React.Component {
                         <Col >
                             <ButtonToolbar>
                                 <Button className="menu-button-small menu-button-purple" href="/cveditor" variant="outline-info">Kreator CV</Button>
-                                <Button className="menu-button-small menu-button-purple" variant="outline-info">Od czego zacząć?</Button>
-                                <Button className="menu-button-small menu-button-purple" variant="outline-info">Oferty pracy</Button>
+                                <Button className="menu-button-small menu-button-purple disabled" variant="outline-info">Od czego zacząć?</Button>
+                                <Button className="menu-button-small menu-button-purple disabled" variant="outline-info">Oferty pracy</Button>
                             </ButtonToolbar>
                         </Col>
                         <Col />
                     </Row>
                 </div>
             </Container>
-
-           /* <div className="menu text-center">
-                <Row className="menu-row">
-
-                    <a href="/cvEditor" className="menu-tile"><Col>Kreator tworzenia CV</Col></a>
-                    <Col className="disabled menu-tile">Od czego zacząć usamodzielnienie</Col>
-                    <Col className="disabled menu-tile">Kursy, staże, praca</Col>
-                    <Col className="disabled menu-tile">Opis stanowisk</Col>
-                </Row>
-                <Row className="menu-row">
-                    <Col className="disabled menu-tile">Testy zawodowe i osobowości</Col>
-                    <Col className="disabled menu-tile">Historie usamodzielnionych</Col>
-                    <Col className="disabled menu-tile">Znani z dorosłości</Col>
-                    <Col className="disabled menu-tile">Jak zarządzać budżetem</Col>
-                </Row>
-            </div>*/
-
         )
     }
 }
