16fc505104337058bbe431687e13e0f515cfa7e3
Damian <7dami77@gmail.com>
2020-02-01 02:48:23 +0100

    Moved personal data form to separate component

---
+++ front-end/src/Views/CVEditorPage/components/PersonalDataTab.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { Button, ButtonToolbar, Form } from "react-bootstrap";
+import movie_1 from "../../../assets/movie_1.png";
+import DatePicker, { registerLocale } from "react-datepicker";
+import polish from "date-fns/locale/pl";
+import { handleBlur, handleBirthDateChange } from 'Views/CVEditorPage/functions/handlers.js';
+
+registerLocale("pl", polish);
+
+class PersonalDataTab extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fullName: "",
+            birthDate: new Date(),
+            phoneNumber: "",
+            email: ""
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Form.Label>
+                    <h3>Dane osobowe</h3>
+                    <br></br>
+                    <img className="cv_section_img" src={movie_1} width="400vw" alt="" />
+                </Form.Label>
+                <Form.Group controlId="personalData">
+                    <Form.Label column={""} sm="2">
+                        Imię i nazwisko:
+                    </Form.Label>
+                    <Form.Control
+                        inline
+                        type="text"
+                        required
+                        defaultValue={this.state.fullName}
+                        placeholder="Jan Przykładowy"
+                        onBlur={e => handleBlur(this, e, "fullName")}
+                        className="cv_page_input"
+                    />
+                    <Form.Label column={""} sm="2">
+                        Data urodzenia:{" "}
+                    </Form.Label>
+                    <DatePicker
+                        className="cv_page_input"
+                        locale="pl"
+                        dateFormat=" dd.MM.yyyy"
+                        selected={this.state.birthDate}
+                        onChange={() => handleBirthDateChange(this)}
+                        withPortal
+                        peekNextMonth
+                        showMonthDropdown
+                        showYearDropdown
+                        dropdownMode="select"
+                    />
+                    <Form.Label column={""} sm="2">
+                        Numer telefonu:
+                    </Form.Label>
+                    <Form.Control
+                        type="text"
+                        defaultValue={this.state.phoneNumber}
+                        placeholder="+48123456789"
+                        onBlur={e => handleBlur(this, e, "phoneNumber")}
+                        className="cv_page_input"
+                    />
+                    <Form.Label column={""} sm="2">
+                        Adres email:
+                    </Form.Label>
+                    <Form.Control
+                        type="email"
+                        required
+                        defaultValue={this.state.email}
+                        placeholder="example@domain.com"
+                        onBlur={e => handleBlur(this, e, "email")}
+                        className="cv_page_input"
+                    />
+                </Form.Group>
+                <ButtonToolbar>
+                    <Button className="form_navigation_prev" disabled>← Wstecz</Button>
+                    <Button
+                        className="form_navigation_next"
+                        onClick={e => this.props.onNextButtonClick()}
+                    >
+                        Dalej →
+                    </Button>
+                </ButtonToolbar>
+            </div>
+        )
+    }
+}
+
+export default PersonalDataTab;
--- front-end/src/Views/CVEditorPage/index.js
+++ front-end/src/Views/CVEditorPage/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import {Button, ButtonToolbar, Card, Container, Form, Tab, Tabs} from "react-bootstrap";
-import DatePicker, { registerLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import polish from "date-fns/locale/pl";
 import bgImage from "../../assets/fot..png";
 import "./style.css";
-import movie_1 from "../../assets/movie_1.png";
 import movie_2 from "../../assets/movie_2.png";
 import movie_3 from "../../assets/movie_3.png";
 import movie_4 from "../../assets/movie_4.png";
@@ -14,22 +11,16 @@ import Col from "react-bootstrap/Col";
 import Languages from 'Views/CVEditorPage/components/Languages';
 import Skills from 'Views/CVEditorPage/components/Skills';
 import RenderForm from 'Views/CVEditorPage/components/RenderForm';
+import PersonalDataTab from 'Views/CVEditorPage/components/PersonalDataTab';
 import {connect} from 'react-redux'
 
-import {handleBlur,handleCVSubmit,handleBirthDateChange,handleAddLanguage,handleLanChange,handleLanLvlChange,handleSkillAdd} from 'Views/CVEditorPage/functions/handlers.js';
+import {handleCVSubmit,handleAddLanguage,handleLanChange,handleLanLvlChange,handleSkillAdd} from 'Views/CVEditorPage/functions/handlers.js';
 
-
-registerLocale("pl", polish);
 
 class CVEditorPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            fullName: "",
-            birthDate: new Date(),
-            phoneNumber: "",
-            email: "",
-
             education: {},
             eduPlace: "",
             eduDescription: "",
@@ -67,70 +58,7 @@ class CVEditorPage extends React.Component {
                             <Col>
                                 <Tabs activeKey={this.state.formTab} onSelect={e => this.setState({ formTab: e })} id="tabs">
                                     <Tab eventKey="personalData" title="Dane osobowe">
-                                        <Form.Label>
-                                            <h3>Dane osobowe</h3>
-                                            <br></br>
-                                            <img className="cv_section_img" src={movie_1} width="400vw" alt="" />
-                                        </Form.Label>
-                                        <Form.Group controlId="personalData">
-                                            <Form.Label column={""} sm="2">
-                                                Imię i nazwisko:
-                                            </Form.Label>
-                                            <Form.Control
-                                                inline
-                                                type="text"
-                                                required
-                                                defaultValue={this.state.fullName}
-                                                placeholder="Jan Przykładowy"
-                                                onBlur={e => handleBlur(this,e, "fullName")}
-                                                className="cv_page_input"
-                                            />
-                                            <Form.Label column={""} sm="2">
-                                                Data urodzenia:{" "}
-                                            </Form.Label>
-                                            <DatePicker
-                                                className="cv_page_input"
-                                                locale="pl"
-                                                dateFormat=" dd.MM.yyyy"
-                                                selected={this.state.birthDate}
-                                                onChange={() => handleBirthDateChange(this)}
-                                                withPortal
-                                                peekNextMonth
-                                                showMonthDropdown
-                                                showYearDropdown
-                                                dropdownMode="select"
-                                            />
-                                            <Form.Label column={""} sm="2">
-                                                Numer telefonu:
-                                            </Form.Label>
-                                            <Form.Control
-                                                type="text"
-                                                defaultValue={this.state.phoneNumber}
-                                                placeholder="+48123456789"
-                                                onBlur={e => handleBlur(this,e, "phoneNumber")}
-                                                className="cv_page_input"
-                                            />
-                                            <Form.Label column={""} sm="2">
-                                                Adres email:
-                                            </Form.Label>
-                                            <Form.Control
-                                                type="email"
-                                                required
-                                                defaultValue={this.state.email}
-                                                placeholder="example@domain.com"
-                                                onBlur={e => handleBlur(this,e, "email")}
-                                                className="cv_page_input"
-                                            />
-                                        </Form.Group>
-                                        <ButtonToolbar>
-                                            <Button className="form_navigation_prev" disabled>← Wstecz</Button>
-                                            <Button
-                                                className="form_navigation_next"
-                                                onClick={e => this.setState({ formTab: "education" })}
-                                            >
-                                                Dalej →
-                                            </Button>
-                                        </ButtonToolbar>
+                                        <PersonalDataTab onNextButtonClick={() => this.setState({ formTab: "education" })}/>
                                     </Tab>
                                     <Tab eventKey="education" title="Edukacja">
                                         <Form.Group id="complex-cv-fields">
