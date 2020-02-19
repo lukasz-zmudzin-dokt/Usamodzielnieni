5f699d008eb39a21c429638eaa411127c2979774
unknown <jakub.swider999@gmail.com>
2020-02-19 14:51:25 +0100

    moved "login form" from index.js to ./components

---
+++ front-end/src/Views/LoginPage/components/loginForm.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Card, Form } from 'react-bootstrap';
+import { onChange } from "../functions/handlers";
+
+class LoginForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: ""
+        };
+        onChange.bind(this);
+    }
+
+    render() {
+        const { username, password } = this.state;
+
+        return (
+            <Card
+                bg="light"
+                className="loginPage__cardSection col-lg mr-lg-3"
+            >
+                <Card.Body className="">
+                    <Form.Group controlId="formGroupUsername">
+                        <Form.Control
+                            type="text"
+                            placeholder="Login"
+                            required
+                            value={username}
+                            onChange={e => onChange(this, e)}
+                            className="loginPage__input"
+                            minLength="6"
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Podaj właściwy login
+                </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group controlId="formGroupPassword">
+                        <Form.Control
+                            type="password"
+                            autoComplete="on"
+                            placeholder="Hasło"
+                            value={password}
+                            onChange={e => onChange(this, e)}
+                            required
+                            minLength="6"
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Podaj właściwe hasło
+                </Form.Control.Feedback>
+                    </Form.Group>
+                </Card.Body>
+            </Card>
+        )
+    }
+
+
+}
+
+export default LoginForm;
--- front-end/src/Views/LoginPage/index.js
+++ front-end/src/Views/LoginPage/index.js
@@ -5,14 +5,15 @@ import Form from "react-bootstrap/Form";
 import { Link, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { setUserToken } from "redux/actions";
+import { handleCheck } from "./functions/handlers";
+import { handleSubmit } from "./functions/handlers";
+import { renderRedirect } from "./functions/handlers";
+import LoginForm from "./components/loginForm";
 import { createMessage } from "./functions/createMessage";
 import { sendData } from "./functions/sendData";
 import { onChange } from "./functions/handlers";
 import { setRedirect } from "./functions/handlers";
 import { setCookie } from "./functions/handlers";
-import { handleCheck } from "./functions/handlers";
-import { handleSubmit } from "./functions/handlers";
-import { renderRedirect } from "./functions/handlers";
 
 import "Views/LoginPage/style.css";
 import bgImage from "../../assets/fot..png";
@@ -63,34 +64,7 @@ class LoginPage extends React.Component {
               onSubmit={e => handleSubmit(this, e)}
               className="primary"
             >
-              <Form.Group controlId="formGroupUsername">
-                <Form.Control
-                  type="text"
-                  placeholder="Login"
-                  required
-                  value={username}
-                  onChange={e => onChange(this, e)}
-                  className="loginPage__input"
-                  minLength="6"
-                />
-                <Form.Control.Feedback type="invalid">
-                  Podaj właściwy login
-    </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group controlId="formGroupPassword">
-                <Form.Control
-                  type="password"
-                  autoComplete="on"
-                  placeholder="Hasło"
-                  value={password}
-                  onChange={e => onChange(this, e)}
-                  required
-                  minLength="6"
-                />
-                <Form.Control.Feedback type="invalid">
-                  Podaj właściwe hasło
-    </Form.Control.Feedback>
-              </Form.Group>
+              <LoginForm />
               <Form.Group controlId="formBasicCheckbox">
                 <Form.Check
                   type="checkbox"
@@ -99,14 +73,13 @@ class LoginPage extends React.Component {
                   label="Zapamiętaj mnie"
                 />
               </Form.Group>
-
               <Button
                 variant="secondary"
                 className="loginPage__button"
                 type="submit"
               >
                 Zaloguj
-  </Button>
+                </Button>
             </Form>
             {incorrect ? (
               <div className="loginPage__messageFail">
