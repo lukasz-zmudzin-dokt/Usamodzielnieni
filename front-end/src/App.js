d1b19b3dec04c1022afedc90a7efa0caa1edb9a0
Damian <7dami77@gmail.com>
2020-02-21 12:50:34 +0100

    added index.js in Views

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,15 @@
 import React from "react";
 import "./App.css";
-import UserProfilePage from "Views/UserProfilePage";
-import LoginPage from "Views/LoginPage";
-import RegisterPage from "Views/RegisterPage";
-import HeaderTemplate from "./Views/PageHeader/headerTemplate";
-import CVEditorPage from "./Views/CVEditorPage";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Footer from "Views/Footer";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "redux/reducer";
 import Cookies from "universal-cookie";
 import { setUserToken } from "redux/actions";
-import Menu from "./Views/Menu";
+import { 
+  UserProfilePage, LoginPage, RegisterPage,
+  HeaderTemplate, CVEditorPage, Menu, Footer
+} from "./Views"
 
 const cookies = new Cookies();
 
---
+++ front-end/src/Views/index.js
@@ -0,0 +1,17 @@
+import UserProfilePage from "./UserProfilePage";
+import LoginPage from "./LoginPage";
+import RegisterPage from "./RegisterPage";
+import HeaderTemplate from "./PageHeader/headerTemplate";
+import CVEditorPage from "./CVEditorPage";
+import Footer from "./Footer";
+import Menu from "./Menu";
+
+export {
+    UserProfilePage,
+    LoginPage,
+    RegisterPage,
+    HeaderTemplate,
+    CVEditorPage,
+    Footer,
+    Menu
+}
