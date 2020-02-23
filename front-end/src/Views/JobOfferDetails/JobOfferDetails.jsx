badce9e3fa4b14a113a1681c73c724c82b8bfc1c
Damian <7dami77@gmail.com>
2020-02-23 15:56:18 +0100

    Added getOfferDetails func

--- front-end/src/Views/JobOfferDetails/JobOfferDetails.jsx
+++ front-end/src/Views/JobOfferDetails/JobOfferDetails.jsx
@@ -1,9 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Container, Card } from "react-bootstrap";
 import "./style.css";
+import { UserContext } from "context";
+
+const getOfferDetails = async (id, token) => {
+  let url = "https://usamo-back.herokuapp.com/offers/.../";
+  const headers = {
+    Authorization: "Token " + token,
+    "Content-Type": "application/json"
+  };
+
+  const response = await fetch(url, { method: "GET", headers });
+
+  if (response.status === 200) {
+    return response.json().then(offer => mapOffer(offer));
+  } else {
+    throw response.status;
+  }
+  // TODO
+}
+
+const mapOffer = (offer) => ({
+  id: offer.id,
+  title: offer.title,
+  description: offer.description
+  // TODO
+})
 
 const JobOfferDetails = props => {
-    const [offer, setOffer] = useState({
+  const [offer, setOffer] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const user = useContext(UserContext)
+
+  useEffect(
+    () => { 
+      loadOffer(props.match.params.id, user.token)  // https://github.com/facebook/react/issues/14326#issuecomment-441680293
+    },
+    [props.match.params.id, user.token]
+  );
+
+  const loadOffer = async (id, token) => {
+    setIsLoading(true);
+    let loadedOffer;
+    try {
+      loadedOffer = await getOfferDetails(id, token);
+    } catch {
+      loadedOffer = {
         id: "1",
         title: "Stolarz poszukiwany!",
         description: "Do naszego zakładu potrzebujemy osoby, która ma chęć rąbać drewno! To możesz być ty!!!",
@@ -12,19 +54,28 @@ const JobOfferDetails = props => {
         lastName: "Psikuta",
         email: "paniewidzisztamsnakońcu@gmail.com",
         phone: "133792137"
-    });
+      } // TODO: dodanie informacji o błędzie
+    }
+    console.log(loadedOffer);
+    setOffer(loadedOffer);
+    setIsLoading(false);
+  }
 
-    return (
-        <Container>
-            <Card>
-            <Card.Header as="h2">Szczegóły oferty pracy</Card.Header>
-            <Card.Body>
-                <h3>{offer.title}</h3>
-                <div>{offer.description}</div>
-            </Card.Body>
-            </Card>
-        </Container>
-    )
+  return (
+    <Container>
+      <Card>
+      <Card.Header as="h2">Szczegóły oferty pracy</Card.Header>
+      <Card.Body>
+        {isLoading ? <div>Ładowanie...</div> : (
+          <div>
+            <h3>{offer.title}</h3>
+            <div>{offer.description}</div>
+          </div>
+        )}
+      </Card.Body>
+      </Card>
+    </Container>
+  )
 }
 
 export default JobOfferDetails;
--- front-end/src/Views/JobOffersPage/JobOffersPage.jsx
+++ front-end/src/Views/JobOffersPage/JobOffersPage.jsx
@@ -4,56 +4,56 @@ import { IndexLinkContainer } from 'react-router-bootstrap';
 import "./style.css";
 
 const JobOfferInfo = ({ offer, ...rest }) => {
-    const getAddressString = (offer) => "TODO";
+  const getAddressString = (offer) => "TODO";
 
-    return (
-        <Row {...rest}>
-            <Col>{offer.title}</Col>
-            <Col>{offer.companyName}</Col>
-            <Col>{getAddressString(offer)}</Col>
-            <Col>
-                <IndexLinkContainer to={`/jobOffers/${offer.id}`}>
-                    <Button>Pokaż szczegóły</Button>
-                </IndexLinkContainer>
-            </Col>
-        </Row>
-    )
+  return (
+    <Row {...rest}>
+      <Col>{offer.title}</Col>
+      <Col>{offer.companyName}</Col>
+      <Col>{getAddressString(offer)}</Col>
+      <Col>
+        <IndexLinkContainer to={`/jobOffers/${offer.id}`}>
+          <Button>Pokaż szczegóły</Button>
+        </IndexLinkContainer>
+      </Col>
+    </Row>
+  )
 }
 
 const JobOffersPage = props => {
-    const [offers, setOffers] = useState([
-        {
-            id: "1",
-            title: "Stolarz poszukiwany!",
-            description: "Do naszego zakładu potrzebujemy osoby, która ma chęć rąbać drewno! To możesz być ty!!!",
-            companyName: "Rębacze z Cintry sp. z o.o.",
-            firstName: "Jarosław",
-            lastName: "Psikuta",
-            email: "paniewidzisztamsnakońcu@gmail.com",
-            phone: "133792137"
-        },
-        {
-            id: "2",
-            title: "Pszczelarz poszukiwany!",
-            description: "Do naszego zakładu potrzebujemy osoby, która ma chęć wyciagac miody pszczolom! To możesz być ty!!!",
-            companyName: "Miody sp. z o.o.",
-            firstName: "Jarosław",
-            lastName: "Psikuta",
-            email: "paniewidzisztamsnakońcu@gmail.com",
-            phone: "133792137"
-        }
-    ]);
+  const [offers, setOffers] = useState([
+    {
+      id: "1",
+      title: "Stolarz poszukiwany!",
+      description: "Do naszego zakładu potrzebujemy osoby, która ma chęć rąbać drewno! To możesz być ty!!!",
+      companyName: "Rębacze z Cintry sp. z o.o.",
+      firstName: "Jarosław",
+      lastName: "Psikuta",
+      email: "paniewidzisztamsnakońcu@gmail.com",
+      phone: "133792137"
+    },
+    {
+      id: "2",
+      title: "Pszczelarz poszukiwany!",
+      description: "Do naszego zakładu potrzebujemy osoby, która ma chęć wyciagac miody pszczolom! To możesz być ty!!!",
+      companyName: "Miody sp. z o.o.",
+      firstName: "Jarosław",
+      lastName: "Psikuta",
+      email: "paniewidzisztamsnakońcu@gmail.com",
+      phone: "133792137"
+    }
+  ]);
 
-    return (
-        <Container>
-            <Card>
-                <Card.Header as="h2">Oferty pracy</Card.Header>
-                <Card.Body>
-                    {offers.map((offer) => <JobOfferInfo offer={offer} />)}
-                </Card.Body>
-            </Card>
-        </Container>
-    );
+  return (
+    <Container>
+      <Card>
+        <Card.Header as="h2">Oferty pracy</Card.Header>
+        <Card.Body>
+          {offers.map((offer) => <JobOfferInfo offer={offer} />)}
+        </Card.Body>
+      </Card>
+    </Container>
+  );
 }
 
 export default JobOffersPage;
