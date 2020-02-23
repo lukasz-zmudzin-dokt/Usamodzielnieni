00d286d7c1ebe1922cbf4742cff45cde76e81d4c
Miko1012 <mczarkowski7@outlook.com>
2020-02-23 12:16:37 +0100

    wstępny wiersz wyświetlania cv

--- front-end/src/Views/CVApprovalPage/CVApprovalPage.jsx
+++ front-end/src/Views/CVApprovalPage/CVApprovalPage.jsx
@@ -1,17 +1,54 @@
 import React from "react";
-import {Card, Container} from "react-bootstrap";
+import {Card, Col, Container, Row} from "react-bootstrap";
 import "./style.css";
+
+const cvs = [
+    {
+        number: 0,
+        firstName: "Maciej",
+        lastName: "Kozłowski",
+        date: "09.18.2019",
+        mail: "maciej.kozlowski@gmail.dom"
+    },
+    {
+        number: 1,
+        firstName: "Ola",
+        lastName: "Kwiatek",
+        date: "11.02.2020",
+        mail: "kwiatola@olakwiat.nl"
+    }
+];
+
+class CVDetails extends React.Component {
+    render() {
+        const { cv } = this.props;
+        return (
+            <Row>
+                <Col>{cv.number}</Col>
+                <Col>{cv.firstName}</Col>
+                <Col>{cv.lastName}</Col>
+                <Col>{cv.date}</Col>
+                <Col>{cv.mail}</Col>
+                <Col>AKCEPTUJ</Col>
+                <Col>POPRAW</Col>
+            </Row>
+        );
+    }
+}
 
 class CVApprovalPage extends React.Component {
     render() {
         return (
             <Container>
-                <Card>
-                    <Card.Header><h3>CV do przejrzenia</h3></Card.Header>
-                    <Card.Body>
-                        <h1>lalala</h1>
-                    </Card.Body>
-                </Card>
+                <div className="cv_approval_background">
+                    <Card className="cv_approval_card">
+                        <Card.Header className="cv_approval_card_title"><h3>CV do przejrzenia</h3></Card.Header>
+                        <Card.Body>
+                            <h1>lalala</h1>
+                            <CVDetails cv={cvs[0]} />
+                        </Card.Body>
+                    </Card>
+                </div>
             </Container>
         )
     };
