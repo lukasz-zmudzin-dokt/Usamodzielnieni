25b02a27f1be61c70bd90d266b2bef218ffb26a0
Miko1012 <mczarkowski7@outlook.com>
2020-02-23 13:16:14 +0100

    prototyp skończony

--- front-end/src/Views/CVApprovalPage/CVApprovalPage.jsx
+++ front-end/src/Views/CVApprovalPage/CVApprovalPage.jsx
@@ -1,36 +1,70 @@
 import React from "react";
-import {Card, Col, Container, Row} from "react-bootstrap";
+import {Button, Card, Col, Container, Row} from "react-bootstrap";
 import "./style.css";
 
 const cvs = [
     {
-        number: 0,
+        id: 0,
         firstName: "Maciej",
         lastName: "Kozłowski",
-        date: "09.18.2019",
-        mail: "maciej.kozlowski@gmail.dom"
+        email: "maciej.kozlowski@gmail.dom"
     },
     {
-        number: 1,
+        id: 1,
         firstName: "Ola",
         lastName: "Kwiatek",
-        date: "11.02.2020",
-        mail: "kwiatola@olakwiat.nl"
+        email: "kwiatola@olakwiat.nl"
     }
 ];
+
+class CVLegend extends React.Component {
+    render() {
+        return (
+            <Row className="cv_legend mb-3">
+                <Col md={1}>ID</Col>
+                <Col md={2}>Imię</Col>
+                <Col md={2}>Nazwisko</Col>
+                <Col md={3}>Email</Col>
+                <Col md={2}>Pobierz</Col>
+                <Col md={2}>Akcje</Col>
+            </Row>
+        );
+    }
+}
+
 
 class CVDetails extends React.Component {
     render() {
         const { cv } = this.props;
+
+        function downloadCV(id) {
+            console.log("pobieram CV o id " + id);
+        }
+
+        function acceptCV(id) {
+            console.log("akceptuję CV o id " + id);
+        }
+        function improveCV(id) {
+            console.log("poprawiam CV o id " + id);
+        }
+
         return (
-            <Row>
-                <Col>{cv.number}</Col>
-                <Col>{cv.firstName}</Col>
-                <Col>{cv.lastName}</Col>
-                <Col>{cv.date}</Col>
-                <Col>{cv.mail}</Col>
-                <Col>AKCEPTUJ</Col>
-                <Col>POPRAW</Col>
+            <Row className="mt-3">
+                <Col md={1}>{cv.id}</Col>
+                <Col md={2}>{cv.firstName}</Col>
+                <Col md={2}>{cv.lastName}</Col>
+                <Col md={3}>
+                    <a href={"mailto:" + cv.email}> {cv.email} </a>
+                </Col>
+                <Col md={2}>
+                    <Button variant="primary" onClick={downloadCV(cv.id)}>Pobierz CV</Button>
+                </Col>
+                <Col md={1}>
+                    <Button variant="success" onClick={acceptCV(cv.id)}>Akceptuj</Button>
+                </Col>
+                <Col md={1}>
+                    <Button variant="warning" onClick={improveCV(cv.id)}>Popraw</Button>
+                </Col>
             </Row>
         );
     }
@@ -44,8 +78,12 @@ class CVApprovalPage extends React.Component {
                     <Card className="cv_approval_card">
                         <Card.Header className="cv_approval_card_title"><h3>CV do przejrzenia</h3></Card.Header>
                         <Card.Body>
-                            <h1>lalala</h1>
-                            <CVDetails cv={cvs[0]} />
+                            <CVLegend />
+                            {cvs.map((value, index) => {
+                                return (
+                                    <CVDetails cv={value} key={value.id}/>
+                                )
+                            })}
                         </Card.Body>
                     </Card>
                 </div>
@@ -53,5 +91,7 @@ class CVApprovalPage extends React.Component {
         )
     };
 }
+
+
 
 export default CVApprovalPage;
