02b9018ca7407931868c5595cfb5910a528af136
Adrian Mostowski <adaxelx@o2.pl>
2020-02-25 17:29:20 +0100

    add default value and 2 new filter options

--- front-end/src/Views/JobOffersPage/_components/Search/Search.jsx
+++ front-end/src/Views/JobOffersPage/_components/Search/Search.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Form, Button, Row } from "react-bootstrap";
 import "./style.css";
 
-const data = [
+const voivodeships = [
   "Zachodnio-pomorskie",
   "Pomorskie",
   "Warmińsko-Mazurskie",
@@ -21,52 +21,41 @@ const data = [
   "Podkarpackie"
 ];
 
+const typeOfWork = ["Szkolenie", "Praca", "Staż"];
+const earningsTab = ["Płatne", "Bezpłatne"];
+
 const Search = ({ offers, setFilter }) => {
-  const [voivodeship, setVoivodeship] = useState(offers[0]["voivodeship"]);
-  const [companyName, setCompanyName] = useState(offers[0]["companyName"]);
+  const [voivodeship, setVoivodeship] = useState("-- Wybierz --");
+  const [companyName, setCompanyName] = useState("-- Wybierz --");
+  const [type, setType] = useState("-- Wybierz --");
+  const [earnings, setEarnings] = useState("-- Wybierz --");
 
   const filter = event => {
     event.preventDefault();
-    const filters = { voivodeship, companyName };
+    const filters = { voivodeship, companyName, type, earnings };
     let newTab = offers;
-
+    let check = 0;
     for (let key in filters) {
-      if (filters[key] !== "") {
+      if (filters[key] !== "-- Wybierz --") {
         newTab = newTab.filter(val => {
           return val[key] === filters[key];
         });
-      }
+      } else check++;
     }
-    setFilter({ tab: newTab, changed: true });
+    if (check != 4) setFilter({ tab: newTab, changed: true });
   };
 
   const deleteFilter = e => {
+    setVoivodeship("-- Wybierz --");
+    setCompanyName("-- Wybierz --");
+    setType("-- Wybierz --");
+    setEarnings("-- Wybierz --");
     setFilter(offers);
   };
   let companies = [...new Set(offers.map(({ companyName }) => companyName))];
   return (
     <Form className="mb-2" onSubmit={filter}>
       <div className="search__wrapper">
-        {/* <Form.Group controlId="exampleForm.ControlSelect2">
-          <Form.Label>Branża</Form.Label>
-          <Form.Control as="select">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </Form.Control>
-        </Form.Group> */}
         <Form.Group controlId="exampleForm.ControlSelect2">
           <Form.Label>Województwo</Form.Label>
           <Form.Control
@@ -74,8 +63,11 @@ const Search = ({ offers, setFilter }) => {
             onChange={e => setVoivodeship(e.target.value)}
             value={voivodeship}
           >
-            {data.map(val => (
-              <option>{val}</option>
+            <option data-hidden="true" disabled>
+              -- Wybierz --
+            </option>
+            {voivodeships.map(val => (
+              <option key={val}>{val}</option>
             ))}
           </Form.Control>
         </Form.Group>
@@ -86,24 +78,57 @@ const Search = ({ offers, setFilter }) => {
             onChange={e => setCompanyName(e.target.value)}
             value={companyName}
           >
+            <option data-hidden="true" disabled>
+              -- Wybierz --
+            </option>
             {companies.map(val => (
-              <option>{val}</option>
+              <option key={val}>{val}</option>
+            ))}
+          </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlSelect2">
+          <Form.Label>Szkolenie/Praca</Form.Label>
+          <Form.Control
+            as="select"
+            onChange={e => setType(e.target.value)}
+            value={type}
+          >
+            <option data-hidden="true" disabled>
+              -- Wybierz --
+            </option>
+            {typeOfWork.map(val => (
+              <option key={val}>{val}</option>
+            ))}
+          </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlSelect2">
+          <Form.Label>Płatne/Bezpłatne</Form.Label>
+          <Form.Control
+            as="select"
+            onChange={e => setEarnings(e.target.value)}
+            value={earnings}
+          >
+            <option data-hidden="true" disabled>
+              -- Wybierz --
+            </option>
+            {earningsTab.map(val => (
+              <option key={val}>{val}</option>
             ))}
           </Form.Control>
         </Form.Group>
       </div>
-      <Row className="w-100 justify-content-center align-items-center m-0">
+      <Row className="justify-content-center align-items-center m-0">
         <Button
           variant="secondary"
           type="submit"
-          className="pl-5 pr-5 pt-2 pb-2"
+          className="pl-4 pr-4 pt-2 pb-2 mr-5"
         >
           Filtruj
         </Button>
         <Button
           variant="secondary"
           onClick={deleteFilter}
-          className="pl-5 pr-5 pt-2 pb-2"
+          className="pl-4 pr-4 pt-2 pb-2"
         >
           Usuń filtr
         </Button>
