85aef6f968dc7f792f17bd506340f9b818868968
Damian <7dami77@gmail.com>
2020-03-11 21:10:32 +0100

    toggle update

--- front-end/src/Views/PageHeader/components/NotificationToggle.jsx
+++ front-end/src/Views/PageHeader/components/NotificationToggle.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Button, Badge } from "react-bootstrap";
 
-const NotificationToggle = props => (
-    <Button variant="light" {...props}>
-        Powiadomienia { props.count > 0 && (<Badge variant="secondary">{props.count}</Badge>) }
+const NotificationToggle = ({ count, ...rest }) => (
+    <Button variant="light" {...rest}>
+        Powiadomienia { count > 0 && (<Badge variant="secondary">{count > 9 ? '9+' : count}</Badge>) }
     </Button>
 )
 
