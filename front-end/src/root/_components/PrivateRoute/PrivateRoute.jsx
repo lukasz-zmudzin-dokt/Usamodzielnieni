2ad8585f26806a78cdd9301d9c9ad1368ccf61bd
Adrian Mostowski <adaxelx@o2.pl>
2020-03-15 11:54:58 +0100

    small refactor(for easier testing)

--- front-end/src/root/_components/PrivateRoute/PrivateRoute.jsx
+++ front-end/src/root/_components/PrivateRoute/PrivateRoute.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import { UserContext } from "context/UserContext";
+
 import { paths } from "constants/routes";
 
-const PrivateRoute = ({ redirect, type, ...rest }) => {
-  return (
-    <UserContext.Consumer>
-      {value => {
-        if (value.token) {
-          if (!type) return <Route {...rest} />;
-          else if (type === value.type) return <Route {...rest} />;
-        }
-        return <Redirect to={redirect} />;
-      }}
-    </UserContext.Consumer>
-  );
+const PrivateRoute = ({ redirect, type, authenticated, ...rest }) => {
+  const checkAuth = () => {
+    if (authenticated.token) {
+      if (!type) return <Route {...rest} />;
+      else if (type === authenticated.type) return <Route {...rest} />;
+    }
+    return <Redirect to={redirect} />;
+  };
+
+  return checkAuth();
 };
 
 PrivateRoute.defaultProps = {
