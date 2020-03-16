e09412006648de14eb10b1bb784ffdd0a4cc539a
Czarek` <mczarkowski7@outlook.com>
2020-03-16 19:48:55 +0100

    tests

--- front-end/src/Views/CVEditorPage/CVEditorPage.test.js
+++ front-end/src/Views/CVEditorPage/CVEditorPage.test.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import CVEditorPage from "./index";
+import renderer from "react-test-renderer";
 
 describe("CVEditor", () => {
     it("should render without crashing", () => {
         render(<CVEditorPage />);
     });
+    it("should look like snapshot", () => {
+        const profile = renderer.create(<CVEditorPage />).toJSON();
+        expect(profile).toMatchSnapshot();
+    })
 
 });
