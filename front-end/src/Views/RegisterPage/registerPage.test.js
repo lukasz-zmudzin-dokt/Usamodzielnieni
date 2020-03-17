ca7b37b6dd197166471f476b4d233c1f46bacea9
balasm <01142551@pw.edu.pl>
2020-03-17 20:42:14 +0100

    added component tests

--- front-end/src/Views/RegisterPage/registerPage.test.js
+++ front-end/src/Views/RegisterPage/registerPage.test.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import RegisterPage from "Views/RegisterPage/index.js"
+import {fireEvent, render, waitForElement} from "@testing-library/react";
+import RegisterPage from "./index.js"
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from 'redux/reducer';
@@ -12,10 +12,21 @@ describe( "RegisterPageTest", () => {
         const store = createStore(reducer);
         render(
             <Provider store={store}>
-                <Router>
+                <MemoryRouter>
                     <RegisterPage />
-                </Router>
+                </MemoryRouter>
             </Provider>);
+    });
+
+    it("onClick should be called", async () => {
+       const onClick = jest.fn();
+       const { component, getByText } = render(<RegisterPage onSubmit={onClick} />);
+       component.setState({
+           userName: '123'
+       });
+       fireEvent.click(getByText('Wyślij'));
+
+       await expect(onClick).toHaveBeenCalledWith('123');
     });
 
     it("should match snapshot", () => {
@@ -29,4 +40,40 @@ describe( "RegisterPageTest", () => {
         );
         expect(container).toMatchSnapshot();
     });
+});
+
+describe("api connection test", () => {
+   let apiFailure;
+   let token;
+   let apiRegistration;
+   beforeAll(() => {
+       token = "123";
+       global.fetch = jest.fn().mockImplementation((input, init) => {
+           return new Promise((resolve, reject) => {
+               if (apiFailure) {
+                   resolve({status: 500});
+               } else if (init.method === "POST") {
+                   resolve({
+                       status: 200,
+                       json: () => Promise.resolve(apiRegistration)
+                   });
+               } else {
+                   reject({});
+               }
+           })
+       })
+   });
+
+   it('should get response from api', async () => {
+      apiFailure = false;
+      const { getByText } = render(
+          <MemoryRouter>
+              <RegisterPage token={token} />
+          </MemoryRouter>
+      );
+
+      fireEvent.click(getByText('Rejestracja', {exact: false}));
+      await waitForElement(() => getByText('Podaj nazwisko'));
+      expect(getByText('Podaj nazwisko')).toBeInTheDocument();
+   });
 });
