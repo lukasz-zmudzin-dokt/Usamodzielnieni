644ed8515c0bd13dbd3561254cb1bab59785d7d5
Damian <7dami77@gmail.com>
2020-03-26 00:12:43 +0100

    jobOffers corrections

--- front-end/src/Views/JobOfferDetails/_components/AddCvForm.jsx
+++ front-end/src/Views/JobOfferDetails/_components/AddCvForm.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Row, Col, Button } from 'react-bootstrap';
+import { Row, Col, Button, Alert } from 'react-bootstrap';
 import { IndexLinkContainer } from 'react-router-bootstrap';
 
 const getCvStatus = async (token) => {
-  let url = "https://usamo-back.herokuapp.com/cvStatus/.../";
+  let url = "https://usamo-back.herokuapp.com/cv/status/";
   const headers = {
     Authorization: "Token " + token,
     "Content-Type": "application/json"
@@ -12,37 +12,74 @@ const getCvStatus = async (token) => {
   const response = await fetch(url, { method: "GET", headers });
 
   if (response.status === 200) {
-    return response.json().status;
+    return response.json().then(res => mapCvStatus(res));
   } else {
     throw response.status;
   }
-  // TODO
 }
 
-const AddCvForm = ({ user, ...props }) => {
+const mapCvStatus = (res) => res.is_verified ? 'VERIFIED' : 'NOT_VERIFIED';
+
+const sendCv = async (id, token) => {
+  let url = `https://usamo-back.herokuapp.com/job/offer-interested/${id}`;
+  const headers = {
+    Authorization: "Token " + token,
+    "Content-Type": "application/json"
+  };
+
+  const response = await fetch(url, { method: "POST", headers });
+
+  if (response.status === 200 || response.status === 400) {
+    return response.status === 200;
+  } else {
+    throw response.status;
+  }
+}
+
+const AddCvForm = ({ id, user, ...props }) => {
   const [cvStatus, setCvStatus] = useState(undefined);
   const [isCvStatusLoading, setIsCvStatusLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-  useEffect(() => { loadCvStatus(user.token) }, [user.token]);
-
-  const loadCvStatus = async (token) => {
-    setIsCvStatusLoading(true);
-    let loadedCvStatus;
-    try {
-      loadedCvStatus = await getCvStatus(token);
-    } catch {
-      loadedCvStatus = "accepted"; // TODO: dodanie informacji o błędzie
+  useEffect(() => {
+    const loadCvStatus = async (token) => {
+      setIsCvStatusLoading(true);
+      let loadedCvStatus;
+      try {
+        loadedCvStatus = await getCvStatus(token);
+      } catch (e) {
+        console.log(e);
+        loadedCvStatus = undefined;
+        setError(true);
+      }
+      setCvStatus(loadedCvStatus);
+      setIsCvStatusLoading(false);
     }
-    setCvStatus(loadedCvStatus);
-    setIsCvStatusLoading(false);
+    loadCvStatus(user.token)
+  }, [user.token]);
+
+  const onApplyClick = async () => {
+    let response;
+    try {
+      response = await sendCv(id, user.token);
+    } catch (e) {
+      console.log(e);
+      setError(true);
+    }
+    const newStatus = response ? 'ADDED' : 'ALREADY_ADDED';
+    setCvStatus(newStatus);
   }
 
-  return (
+  const msg = error ? { variant: "danger", value: "Wystąpił błąd podczas ładowania." }:
+              isCvStatusLoading ? { variant: "info", value: "Ładowanie..." }:
+              cvStatus === 'ADDED' ? { variant: "success", value: "Pomyślnie zaaplikowano do ogłoszenia." }:
+              cvStatus === 'ALREADY_ADDED' && { variant: "danger", value: "Już zaaplikowano do danego ogłoszenia." };
+
+  return msg ? <Alert className="mb-0" variant={msg.variant}>{msg.value}</Alert> : (
     <Row>
-    {isCvStatusLoading ? <Col>Ładowanie...</Col> : (
       <Col>
-        {cvStatus === "accepted" ? (
-          <Button>
+        {cvStatus === "VERIFIED" ? (
+          <Button onClick={onApplyClick}>
             Aplikuj
           </Button>
         ) : (
@@ -53,7 +90,6 @@ const AddCvForm = ({ user, ...props }) => {
           </IndexLinkContainer>
         )}
       </Col>
-    )}
     </Row>
   )
 }
--- front-end/src/Views/JobOffersPage/_components/JobOfferInfo.jsx
+++ front-end/src/Views/JobOffersPage/_components/JobOfferInfo.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Row, Col, Button } from 'react-bootstrap';
 import { IndexLinkContainer } from 'react-router-bootstrap';
-import DetailsItem from './DetailsItem';
+import { DetailsItem } from 'components';
 
 const JobOfferInfo = ({ offer, ...rest }) => {
     return (
       <Row {...rest}>
         <Col>
           <h5>{offer.title}</h5>
-          <Row className="mb-3">
-            <DetailsItem label="Nazwa firmy" value={offer.companyName} />
-            <DetailsItem label="Lokalizacja" value={offer.voivodeship} />
-            <DetailsItem label="Ważne do" value={offer.expirationDate} />
+          <Row>
+            <DetailsItem md="4" xl="3" label="Nazwa firmy">{offer.companyName}</DetailsItem>
+            <DetailsItem md="4" xl="3" label="Lokalizacja">{offer.voivodeship}</DetailsItem>
+            <DetailsItem md="4" xl="3" label="Ważne do">{offer.expirationDate}</DetailsItem>
           </Row>
           <IndexLinkContainer to={`/jobOffers/${offer.id}`}>
             <Button>Pokaż szczegóły</Button>
