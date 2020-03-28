74ad56ffe0b81fd70f0f2c946ca3e0b4833ef6d5
Damian <7dami77@gmail.com>
2020-03-28 18:41:40 +0100

    PersonalDataTab tests

---
+++ front-end/src/Views/CVEditorPage/components/PersonalDataTab/PersonalDataTab.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import PersonalDataTab from './PersonalDataTab';
+
+jest.mock('../', () => ({ CVEditorTab: ({children}) => children }))
+
+describe('ItemsList', () => {
+    let props;
+    beforeEach(() => {
+        props = {
+            data: {
+                firstName: "Jan",
+                lastName: "Kowalski",
+                birthDate: new Date(),
+                phoneNumber: "+48123123123",
+                email: "abc@abc.com"
+            },
+            onChange: () => {}
+        }
+    });
+
+    it('should render without crashing', () => {
+        const { container } = render(
+            <PersonalDataTab {...props} />
+        );
+
+        expect(container).toMatchSnapshot();
+    });
+    it('should not render when data is null', () => {
+        props.data = null
+        const { queryByText } = render(
+            <PersonalDataTab {...props} />
+        );
+
+        expect(queryByText('Imię:')).not.toBeInTheDocument();
+    });
+});
