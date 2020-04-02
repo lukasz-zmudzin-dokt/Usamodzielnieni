9d6ae2018bc7951a3f6cd03dc294885b90029daf
Miko1012 <mczarkowski7@outlook.com>
2020-04-02 03:18:36 +0200

    center div fix

--- front-end/src/Views/CVApprovalPage/CVApprovalPage.jsx
+++ front-end/src/Views/CVApprovalPage/CVApprovalPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Card, Container } from "react-bootstrap";
 import "./style.css";
 import { getCVs } from "./functions/getCVs";
-import {UserContext} from "../../context/UserContext";
+import {UserContext} from "context/UserContext";
 import CVApprovalBody from "./components/CVApprovalBody";
 
 class CVApprovalPage extends React.Component {
@@ -19,12 +19,14 @@ class CVApprovalPage extends React.Component {
     render() {
         return (
             <Container fluid>
+                <div className="max-height">
                     <Card className="center">
                         <Card.Header className="border pb-4"><h3>CV do przejrzenia</h3></Card.Header>
                         <Card.Body className="border">
                             <CVApprovalBody cvs={this.state.cvs} />
                         </Card.Body>
                     </Card>
+                </div>
             </Container>
         )
     };
