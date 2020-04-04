94e7839f0f3941248b601c4db335b76fdd897527
Adrian Mostowski <adaxelx@o2.pl>
2020-04-04 09:05:34 +0200

    add basic blog component structure

--- front-end/src/Views/BlogPage/BlogPage.jsx
+++ front-end/src/Views/BlogPage/BlogPage.jsx
@@ -1,8 +1,15 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Card } from "react-bootstrap";
 
 const BlogPage = () => {
-  return <></>;
+  return (
+    <Container>
+      <Card>
+        <Card.Header as="h2">Blogi</Card.Header>
+        <Card.Body></Card.Body>
+      </Card>
+    </Container>
+  );
 };
 
 export default BlogPage;
