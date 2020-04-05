77ef41d309e820eb95ce36cc99658965b1cf0098
Damian <7dami77@gmail.com>
2020-04-05 21:27:46 +0200

    custom scss

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
+import './custom.scss';
 import App from "root/App";
 import * as serviceWorker from "./serviceWorker";
 
