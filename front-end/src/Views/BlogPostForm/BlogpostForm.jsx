f65bcf5997e5cbe6eb66d220346f4a4fcc33628f
balasm <01142551@pw.edu.pl>
2020-04-07 00:51:03 +0200

    added first few forms

--- front-end/src/Views/BlogPostForm/BlogpostForm.jsx
+++ front-end/src/Views/BlogPostForm/BlogpostForm.jsx
@@ -1,16 +1,49 @@
 import React from "react";
-import {Card, Container, Form} from "react-bootstrap";
+import {Card, Col, Container, Form, Row} from "react-bootstrap";
+import {Editor, createEditorState} from 'medium-draft';
+import {getFilters} from "./functions/fetchData";
+import {FormRow} from "react-bootstrap/Form";
 
 class BlogPostForm extends React.Component {
   constructor(props) {
       super(props);
       this.fileInput = React.createRef();
       this.state = {
-          photo: null
+          photo: null,
+          editorState: null,
+          title: undefined,
+          category: "",
+          filters: {}
       }
   }
 
-  onPhotoChange = () => {
+  componentDidMount() {
+      if (this.props.data !== undefined) {
+          this.setState({
+              editorState: createEditorState(this.props.data.content)
+          });
+      } else {
+          this.setState({
+              editorState: createEditorState()
+          });
+      }
+      this.loadFilters();
+  }
+
+  loadFilters = async () => {
+      let res;
+      try {
+          res = await getFilters(this.context.token);
+      } catch(e) {
+          console.log(e);
+          res = {categories: [], tags: []}
+      }
+      this.setState({
+          filters: res
+      })
+  };
+
+    onPhotoChange = () => {
       this.setState({
           photo: this.fileInput.files[0]
       })
@@ -41,7 +74,33 @@ class BlogPostForm extends React.Component {
                       </Form.Group>
                   </Card.Header>
                   <Card.Body>
+                      <Form.Group controlId="blogpost_title">
+                          <Form.Control
+                            name="title"
+                            className="blogpost_title_form"
+                            placeholder="Wpisz tytuł posta..."
+                            size="lg"
+                            onChange={this.onChange}
+                          />
+                      </Form.Group>
+                      <Form.Row className="categories mx-1 inline">
+                          <Form.Group controlId="blogspot_category">
+                              <Form.Control
+                                as="select"
+                                name="category"
+                                placeholder="Wybierz kategorię z listy..."
+                                array={this.state.filters.categories}
+                                onSelect={this.onChange}
+                              />
+                              <div className="mx-2">lub</div>
+                              <Form.Control
+                                name="category"
+                                placeholder="Wpisz nową kategorię"
+                              />
+                          </Form.Group>
 
+
+                      </Form.Row>
                   </Card.Body>
               </Card>
           </Container>
---
+++ front-end/src/Views/BlogPostForm/functions/fetchData.js
@@ -0,0 +1,23 @@
+export const getFilters = async token => {
+    const urlC = "http://usamo-back.herokuapp.com/blog/categories/";
+    const urlT = "http://usamo-back.herokuapp.com/blog/tags/";
+    const headers = {
+        Authorization: "Token " + token,
+        "Content-Type": "application/json"
+    };
+
+    const responseC = await fetch(urlC, { method: "GET", headers });
+    const responseT = await fetch(urlT, { method: "GET", headers });
+
+    if (responseT.status === 200 && responseC.status === 200) {
+        const tags = await responseT.json().then(res => res);
+        const categories = await responseC.json().then(res => res);
+        const filters = {
+            tags,
+            categories
+        };
+        return filters;
+    } else {
+        throw responseT.status;
+    }
+};
