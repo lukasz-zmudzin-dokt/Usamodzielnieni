67b9bc08c6739ad3006641f3fd89c93885b8b0b3
michalkaniewski <michalkaniewski99@gmail.com>
2020-04-06 14:20:41 +0200

    login form tests & login page fail tests

--- front-end/src/Views/LoginPage/components/loginForm.test.js
+++ front-end/src/Views/LoginPage/components/loginForm.test.js
@@ -1,12 +1,44 @@
 import React from "react";
 import LoginForm from "./loginForm";
 import {MemoryRouter} from "react-router-dom";
-import {render} from "@testing-library/react"
+import {render, fireEvent, getAllByPlaceholderText} from "@testing-library/react"
 import {UserProvider} from "context/UserContext";
+import JobOfferDetails from "Views/JobOfferDetails/JobOfferDetails";
 
 describe('LoginForm', () => {
+    let props;
 
-   it('should render correctly', () => {
+    beforeAll(() => {
+        props = {
+            data: "",
+            onBlur: jest.fn()
+        }
+    });
+
+    it('should match snapshot', () => {
+        const {container} = render(<LoginForm {...props} />);
+
+        expect(container).toMatchSnapshot();
+    });
+
+    it('should call onblur with correct credentials', () => {
+        const {container, getByPlaceholderText} = render(<LoginForm {...props} />);
+        const loginInput = getByPlaceholderText("Login");
+        const passwordInput = getByPlaceholderText("Hasło");
+        fireEvent.change(loginInput, {target: {
+            name: "username",
+            value: "testusername"
+            }
+        });
+        fireEvent.change(passwordInput, {target: {
+            name: "password",
+            value: "testuserpassword"
+            }
+        });
+        expect(props.onBlur).toHaveBeenCalledWith(...props.data, {username: "testusername"});
+        expect(props.onBlur).toHaveBeenCalledWith(...props.data, {password: "testuserpassword"});
+    });
+/*   it('should render correctly', () => {
        const data = {
            username: "qweqwe",
            password: "123123"
@@ -23,5 +55,5 @@ describe('LoginForm', () => {
        );
 
        expect(container).toMatchSnapshot();
-   });
+   });*/
 });
--- front-end/src/Views/LoginPage/loginPage.test.js
+++ front-end/src/Views/LoginPage/loginPage.test.js
@@ -1,6 +1,7 @@
 import React from "react";
-import {fireEvent, render} from "@testing-library/react";
+import {fireEvent, render } from "@testing-library/react";
 import LoginPage from "Views/LoginPage/index.js"
+import {sendData} from "Views/LoginPage/functions/sendData";
 
 import {MemoryRouter} from 'react-router-dom';
 import { UserProvider } from "context";
@@ -11,27 +12,41 @@ import LoginForm from "./components/loginForm";
 describe( "LoginPageTest", () => {
     let credentials;
     let apiFail;
+    let wrongCredentials;
 
     beforeAll(() => {
-        global.fetch = jest.fn().mockImplementation((input, init) => {
-           return new Promise((resolve, reject) => {
-               if (apiFail) {
-                   resolve({status: 500});
-               }
-               switch(init.method) {
-                   case "POST":
-                       resolve({status: 200});
-                       break;
-                   default:
-                       reject({});
-                       break;
-               }
-           });
-        });
+        global.fetch = jest.fn().mockImplementation(() => {
+            return new Promise((resolve, reject) => {
+                if (apiFail) {
+                    resolve({status: 500});
+                }
+                else if(wrongCredentials) {
+                    resolve({status: 400});
+                } else {
+                    switch(init.method) {
+                        case "POST":
+                                resolve({
+                                    status: 201,
+                                    json: async() => new Promise((resolve, reject) => {
+                                        resolve({
+                                            token: "8881f9eb8f03571a40f742e73b799cb3accd1e9d",
+                                            type: "Staff"
+                                        })
+                                    })
+                                });
+                            break;
+                        default:
+                            reject({});
+                            break;
+                     }
+                 }
+            });
+         });
     });
 
     beforeEach(() => {
         apiFail = false;
+        wrongCredentials = false;
         credentials  = {
             username: "qweqwe",
             password: "123123"
@@ -45,31 +60,84 @@ describe( "LoginPageTest", () => {
                 <MemoryRouter>
                     <LoginPage />
                 </MemoryRouter>
-            </UserProvider>
-        );
+            </UserProvider>);
         expect(container).toMatchSnapshot();
     });
 
-    it('should load error message', async() => {
-        const {getByPlaceholderText, getByTestId} = render(
+    it('should show wrong credentials alert', async() => {
+        wrongCredentials = true;
+        const {container, getByPlaceholderText, getByText, getByTestId} = render(
             <UserProvider>
                 <MemoryRouter>
                     <LoginPage />
                 </MemoryRouter>
             </UserProvider>
         );
+        //container.handleIncorrectResponse = jest.fn();
 
-        fireEvent.change(getByPlaceholderText("Login", {exact: false}), {
+        fireEvent.change(getByPlaceholderText("Login"), {
             target: {value: "qweqwe"}
         });
-        fireEvent.change(getByPlaceholderText("Hasło", {exact: false}), {
+        fireEvent.change(getByPlaceholderText("Hasło"), {
             target: {value: "qweqwe"}
         });
 
         fireEvent.click(getByTestId("loginBtn"));
-        await waitForElement(() => getByTestId("login_msgFail", {exact: false}));
-        expect(getByTestId("login_msgFail", {exact: false})).toBeInTheDocument();
+        await waitForElement(() => getByText("Niepoprawny login lub hasło."));
+        //expect(container.handleIncorrectResponse).toHaveBeenCalled();
+        expect(getByText("Niepoprawny login lub hasło.")).toBeInTheDocument();
     });
+
+    it('should show server error alert', async() =>{
+        apiFail = true;
+        const {container, getByPlaceholderText, getByText, getByTestId} = render(
+            <UserProvider>
+                <MemoryRouter>
+                    <LoginPage />
+                </MemoryRouter>
+            </UserProvider>
+        );
+        //container.renderRedirect = jest.fn();
+
+        fireEvent.change(getByPlaceholderText("Login"), {
+            target: {value: "qweqwe"}
+        });
+        fireEvent.change(getByPlaceholderText("Hasło"), {
+            target: {value: "qweqwe"}
+        });
+
+        fireEvent.click(getByTestId("loginBtn"));
+        await waitForElement(() => getByText("Błąd serwera. Proszę spróbować później."));
+        //expect(container.renderRedirect).not.toHaveBeenCalled();
+        expect(getByText("Błąd serwera. Proszę spróbować później.")).toBeInTheDocument();
+    });
+
+    it('should call renderRedirect method', async () => {
+        const { container, getByTestId, getByPlaceholderText} = render(
+            <UserProvider>
+                <MemoryRouter>
+                    <LoginPage />
+                </MemoryRouter>
+            </UserProvider>);
+
+        container.renderRedirect = jest.fn();
+        
+        fireEvent.change(getByPlaceholderText("Login"), {
+            target: {value: "staff1"}
+        });
+        fireEvent.change(getByPlaceholderText("Hasło"), {
+            target: {value: "staff1"}
+        });
+
+        fireEvent.click(getByTestId("loginBtn"));
+        await waitForElement(() => container.renderRedirect);
+        expect(container.renderRedirect).toHaveBeenCalled();
+        //await waitForElement(() => getByTestId('redirect'));
+    });
+});
+
+
+ /*   
 
     it('should call setRedirect method', async () => {
         const {component, getByTestId} = render(
@@ -99,30 +167,5 @@ describe( "LoginPageTest", () => {
         expect(component.renderRedirect()).toHaveBeenCalled();
     });
 
-    it('should render alert when api fails', async () => {
-        apiFail = true;
-        const state = {
-            credentials: credentials,
-            validated: false,
-            incorrect: false
-        };
-
-        const {getByTestId, getByText} = render(
-            <UserProvider>
-                <MemoryRouter>
-                    <LoginPage state={state}/>
-                </MemoryRouter>
-            </UserProvider>
-        );
-
-        const handleIncorrectResponse = jest.fn(500);
-
-        const msgFail = "Błąd serwera.";
-
-        fireEvent.click(getByTestId("loginBtn", {exact: false}));
-        await waitForElement(() => getByText(msgFail, {exact: false}));
-
-        expect(handleIncorrectResponse).toHaveBeenCalled();
-        expect(msgFail).toBeInTheDocument();
-    })
-});
+    
+    */
