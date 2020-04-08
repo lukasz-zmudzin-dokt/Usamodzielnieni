c4e58db9eafc45ab038cd21146a51531c6933a5e
Miko1012 <mczarkowski7@outlook.com>
2020-04-08 14:43:56 +0200

    added loading alert

--- front-end/src/Views/CVApprovalPage/CVApprovalPage.jsx
+++ front-end/src/Views/CVApprovalPage/CVApprovalPage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {Alert, Card, Container} from "react-bootstrap";
-import "./style.css";
 import { getCVs } from "./functions/getCVs";
 import {UserContext} from "context/UserContext";
 import CVApprovalBody from "./components/CVApprovalBody";
@@ -19,12 +18,13 @@ class CVApprovalPage extends React.Component {
                 big: "",
                 small: "",
                 smaller: ""
-            }
+            },
+            loading: true
         };
     }
 
     componentDidMount() {
-        getCVs(this.context.token).then(response => response.status === "200:OK" ? this.setState({ cvs: response.result}) : this.setState({errors: {big: true}, errorMessages: {big : response.status}}));
+        getCVs(this.context.token).then(response => response.status === "200:OK" ? this.setState({ loading: false, cvs: response.result}) : this.setState({loading: false, errors: {big: true}, errorMessages: {big : response.status}}));
         //getCVs(this.context.token).then(response => console.log(response));
     }
 
@@ -32,24 +32,27 @@ class CVApprovalPage extends React.Component {
         const {
             cvs,
             errors,
-            errorMessages
+            errorMessages,
+            loading
         } = this.state;
         return (
             <Container className="mt-4">
-                <div className="width-100">
                     <Card>
-                        <Card.Header className="border pb-4"><h3>CV do przejrzenia</h3></Card.Header>
-                        <Card.Body className="border p-0 pt-3">
-                            {errors.big ? (
-                                <Alert variant="danger">
-                                    Ups, coś poszło nie tak. Kod błędu - {errorMessages.big.status}
-                                </Alert>
-                            ) : (
-                                <CVApprovalBody cvs={cvs} token={this.context.token} component={this}/>
-                            )}
-                        </Card.Body>
+                        <Card.Header className="border pb-4"><h2>CV do przejrzenia</h2></Card.Header>
+                        {loading ? (
+                            <Alert variant="info" className="m-3">Ładuję...</Alert>
+                        ): (
+                            <Card.Body className="border p-0 pt-3">
+                                {errors.big ? (
+                                    <Alert variant="danger">
+                                        Ups, coś poszło nie tak. Kod błędu - {errorMessages.big.status}
+                                    </Alert>
+                                ) : (
+                                    <CVApprovalBody cvs={cvs} token={this.context.token} component={this}/>
+                                )}
+                            </Card.Body>
+                        )}
                     </Card>
-                </div>
             </Container>
         )
     };
