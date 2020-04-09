70fb0e4fd9d4dcfe337d0a3d8a2768057d541aa2
unknown <jakub.swider999@gmail.com>
2020-04-09 16:32:56 +0200

    Revert "Reverted chenges in Views/UserProfilePage subdirectory to master"
    This reverts commit de2313db48e6c1fdf69c1c2f5181d52802337c60.

---
+++ front-end/src/Views/UserProfilePage/components/UserDetails/UserDetails.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { ListGroup } from "react-bootstrap";
+import UserProperty from "Views/UserProfilePage/components/UserProperty";
+
+const UserDetails = props => {
+  const { user, names } = props;
+  const ignore = ["firstName", "lastName", "role"];
+
+  const userProperties = Object.keys(user).map(key => {
+    return !ignore.find(i => i === key) ? (
+      <UserProperty key={key} user={user} property={key} names={names} />
+    ) : null;
+  });
+
+  return <ListGroup className="list-group-flush">{userProperties}</ListGroup>;
+};
+
+export default UserDetails;
