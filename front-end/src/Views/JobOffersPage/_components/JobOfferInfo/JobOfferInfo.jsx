b743813701228dba32b70c564a466cb5ce371523
Miko1012 <mczarkowski7@outlook.com>
2020-04-17 03:58:37 +0200

    branch upload

--- front-end/src/Views/JobOffersPage/_components/JobOfferInfo/JobOfferInfo.jsx
+++ front-end/src/Views/JobOffersPage/_components/JobOfferInfo/JobOfferInfo.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
-import { Row, Col, Button } from 'react-bootstrap';
+import React, {useState} from 'react';
+import {Row, Col, Button, Alert} from 'react-bootstrap';
 import { IndexLinkContainer } from 'react-router-bootstrap';
 import { DetailsItem } from 'components';
+import { deleteOffer } from "Views/JobOffersPage/functions/deleteOffer";
 
-const JobOfferInfo = ({ offer, ...rest }) => {
+const handleDeleteOffer = async (e, id, token, delError, setDelError, success, setSuccess ) => {
+    e.preventDefault();
+    try {
+        await deleteOffer(id, token);
+        setSuccess(true);
+    } catch(err) {
+        console.log(err);
+        setDelError(true);
+    }
+};
+
+const renderDelButton = (id, user, delError, setDelError, success, setSuccess) => {
+    if(user.type === 'Staff') {
+        return (
+            <Button variant="danger" className="ml-3" onClick={e => handleDeleteOffer(e, id, user.token, delError, setDelError, success, setSuccess)}>Usuń ofertę</Button>
+        )
+    }
+};
+
+const JobOfferInfo = ({ context, offer, ...rest }) => {
+    const [delError, setDelError] = useState(false);
+    const [success, setSuccess] = useState(false);
     return (
       <Row {...rest}>
         <Col>
@@ -16,9 +38,14 @@ const JobOfferInfo = ({ offer, ...rest }) => {
           <IndexLinkContainer to={`/jobOffers/${offer.id}`}>
             <Button>Pokaż szczegóły</Button>
           </IndexLinkContainer>
+            {renderDelButton(offer.id, context, delError, setDelError, success, setSuccess)}
+            {
+                delError ? <Alert variant="danger" className="mt-3">Wystąpił błąd podczas usuwania oferty.</Alert> :
+                    success ? <Alert variant="info" className="mt-3">Ta oferta została usunięta.</Alert> : null
+            }
         </Col>
       </Row>
     )
-}
+};
 
 export default JobOfferInfo;
---
+++ front-end/src/Views/JobOffersPage/functions/deleteOffer.js
@@ -0,0 +1,15 @@
+export const deleteOffer = async (id, token) => {
+    let url = `https://usamo-back.herokuapp.com/job/job-offer/${id}/`;
+    const headers = {
+        Authorization: "Token " + token,
+        "Content-Type": "application/json"
+    };
+
+    const response = await fetch(url, {method: "DELETE", headers});
+    await fetch(url, {method: "DELETE", headers});
+    if (response.status === 200) {
+        return response.status;
+    } else {
+        throw response.status;
+    }
+};
