ba37f75105e0c602438e4d6b91d8c68f9546bd12
balasm <01142551@pw.edu.pl>
2020-04-24 10:53:27 +0200

    fixed test date, snapshot update

--- front-end/src/Views/BlogPost/_components/BlogContent/blogContent.test.js
+++ front-end/src/Views/BlogPost/_components/BlogContent/blogContent.test.js
@@ -37,14 +37,14 @@ describe('BlogContent', () => {
            },
            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel tempus metus leo non est. Etiam sit amet lectus quis est congue mollis. Phasellus congue lacus eget neque. Phasellus ornare, ante vitae consectetuer consequat, purus sapien ultricies dolor, et mollis pede metus eget nisi. Praesent sodales velit quis augue. Cras suscipit, urna at aliquam rhoncus, urna quam viverra nisi, in interdum massa nibh nec erat.",
            tags: ["tag1", "tag2", "tag3"],
-           creationDate: "2020-01-02qweqweqwe",
+           creationDate: "02-02-2020qweqweqwe",
            comments: [{
                author: {
                    firstName: "Artysta",
                    lastName: "Malarz",
                    email: "a@m.com"
                },
-               creationDate: "2020-01-01",
+               creationDate: "01-01-2019",
                content: "Witam w nowy rok!",
                id: 1
            }]
