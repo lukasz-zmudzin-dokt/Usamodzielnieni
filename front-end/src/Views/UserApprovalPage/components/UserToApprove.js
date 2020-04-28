aeb50695318e837967a1ade1ca78b4c0226841e5
Miko1012 <mczarkowski7@outlook.com>
2020-04-28 03:55:55 +0200

    removed comments

--- front-end/src/Views/UserApprovalPage/components/UserToApprove.js
+++ front-end/src/Views/UserApprovalPage/components/UserToApprove.js
@@ -120,46 +120,6 @@ const UserToApprove = ({ user }) => {
             </Card.Body>
         );
     }
-
-    /*return (
-        <Card className="border-left-0 border-right-0 border-bottom-0 ">
-            <Accordion.Toggle as={Card.Header} eventKey={user.id} onClick={e => loadUserDetails(e, context.token, user.id)}>
-                {user.username} ({user.type})
-            </Accordion.Toggle>
-            <Accordion.Collapse eventKey={user.id}>
-                {message ? message : (
-                    <Card.Body>
-                        <ListGroup variant="flush">
-                            <ListGroup.Item>
-                                <Row>
-                                    <DetailsItem md={4} label="Nazwa użytkownika">{userDetails.username}</DetailsItem>
-                                    <DetailsItem label="Imię">{userDetails.first_name}</DetailsItem>
-                                    <DetailsItem label="Nazwisko">{userDetails.last_name}</DetailsItem>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Row>
-                                    <DetailsItem label="Email">{userDetails.email}</DetailsItem>
-                                    <DetailsItem label="Telefon">{userDetails.phone_number}</DetailsItem>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Row>
-                                    {address}
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Row className="justify-content-center">
-                                    <Button onClick={e => approveUser(e, context.token, user.id)} variant="success">Akceptuj</Button>
-                                    <Button onClick={e => rejectUser(e, context.token, user.id)} variant="danger" className="ml-3">Odrzuć</Button>
-                                </Row>
-                            </ListGroup.Item>
-                        </ListGroup>
-                    </Card.Body>)
-                }
-            </Accordion.Collapse>
-        </Card>
-    );*/
 };
 
 export default UserToApprove;
