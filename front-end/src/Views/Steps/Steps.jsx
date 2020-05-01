49c18b4322c4d0a4d13a22e6d473dd63c5b2bdea
Damian <7dami77@gmail.com>
2020-05-01 21:50:46 +0200

    state

--- front-end/src/Views/Steps/Steps.jsx
+++ front-end/src/Views/Steps/Steps.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import {
     ProgressBar,
@@ -6,15 +6,15 @@ import {
 } from './components';
 
 const Steps = () => {
-    const steps = [
+    const [steps, setSteps] = useState([
         { id: '1', type: 'main', title: 'abc', value: 'asdasda', next: ['2', '5'] },
         { id: '2', type: 'main', title: 'sdf', value: 'asdasd asda', next: ['3'] },
         { id: '3', type: 'sub', title: 'weq', value: 'zxcz xczx', next: ['4'] },
         { id: '4', type: 'sub', title: 'vxc', value: 'asd asd a', next: ['5'] },
         { id: '5', type: 'main', title: '324', value: 'asdas d1', next: ['6'] },
         { id: '6', type: 'sub', title: 'tsx', value: 's d1w 1' },
-    ];
-    const path = ['1', '2', '3', '4'];
+    ]);
+    const [path, setPath] = useState(['1', '2', '3', '4']);
 
     return (
         <Container>
