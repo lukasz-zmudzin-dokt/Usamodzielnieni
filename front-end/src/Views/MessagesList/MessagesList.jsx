1016201b312967cb9bf585072deeea4d40fea63e
Adrian Mostowski <adaxelx@o2.pl>
2020-05-05 16:25:31 +0200

    create MessageItem component

--- front-end/src/Views/MessagesList/MessagesList.jsx
+++ front-end/src/Views/MessagesList/MessagesList.jsx
@@ -1,39 +1,44 @@
 import React from "react";
-import {ListGroup,Container} from 'react-bootstrap'
+import { ListGroup, Container, Card } from "react-bootstrap";
+import MessageItem from "./_components/MessageItem";
 
 const MessagesList = () => {
+  const data = [
+    {
+      content:
+        "Info długa wiadomość bardzo jakaś dzień dobry i te sprawy co tam jak tam jeszcze trochę żeby się zawinęło albo jeszcze trochę",
+      send: "11:55 12.03.2020",
+      side: "left",
+    },
+    {
+      content:
+        "Info długa wiadomość bardzo jakaś dzień dobry i te sprawy co tam jak tam jeszcze trochę żeby się zawinęło albo jeszcze trochę",
+      send: "11:55 12.03.2020",
+      side: "right",
+    },
+    {
+      content: "a",
+      send: "11:55 12.03.2020",
+      side: "right",
+    },
+    {
+      content: "b",
+      send: "11:55 12.03.2020",
+      side: "left",
+    },
+  ];
   return (
     <Container>
-      <ListGroup>
-        <ListGroup.Item
-          className="messagesList__item messagesList__item--left"
-          variant="primary"
-        >
-          Info długa wiadomość bardzo jakaś dzień dobry i te sprawy co tam jak
-          tam jeszcze trochę żeby się zawinęło albo jeszcze trochę
-          <small class="text-muted">Wysłano: 11:55 12.03.2020</small>
-        </ListGroup.Item>
-        <ListGroup.Item
-          className="messagesList__item messagesList__item--right"
-          variant="info"
-        >
-          Tutaj te długa wiadomość ale nie az tak dluga jak tamta wyzej no
-          jeszcze troszkę albo i tak długa
-          <small class="text-muted">Wysłano: 12:55 12.03.2020</small>
-        </ListGroup.Item>
-        <ListGroup.Item
-          className="messagesList__item messagesList__item--right"
-          variant="info"
-        >
-          a<small class="text-muted">Wysłano: 12:56 12.03.2020</small>
-        </ListGroup.Item>
-        <ListGroup.Item
-          className="messagesList__item messagesList__item--left"
-          variant="primary"
-        >
-          b<small class="text-muted">Wysłano: 11:55 12.03.2020</small>
-        </ListGroup.Item>
-      </ListGroup>
+      <Card>
+        <Card.Header>Rozmowa z X</Card.Header>
+        <Card.Body>
+          <ListGroup>
+            {data.map(({ content, send, side }) => (
+              <MessageItem content={content} send={send} side={side} />
+            ))}
+          </ListGroup>
+        </Card.Body>
+      </Card>
     </Container>
   );
 };
