9b8eab52ce9efae686c1e6dd9afa0eb64ad5efe6
Adrian Mostowski <adaxelx@o2.pl>
2020-05-06 23:17:37 +0200

    fix bugs with state

--- front-end/src/Views/CVCorrection/_components/CVRender/CVRender.jsx
+++ front-end/src/Views/CVCorrection/_components/CVRender/CVRender.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import proxy from "config/api";
 import { Alert } from "react-bootstrap";
@@ -17,13 +17,13 @@ const CVRender = ({ url }) => {
   const [pages, setPages] = useState(1);
   const [activePage, setActivePage] = useState(1);
 
+  const file = useMemo(() => ({ url: `${proxy.plain}${url}` }), [url]);
+
   return (
     <div className="cvCorrection__pdfContainer">
       <Document
         className="CVCorrection__pdf"
-        file={{
-          url: `${proxy.plain}${url}`,
-        }}
+        file={file}
         error={<Alert variant="danger">Nie udało się załadować CV.</Alert>}
         loading={<Alert variant="info">Ładowanie...</Alert>}
         onLoadSuccess={(pdf) => setPages(pdf.numPages)}
