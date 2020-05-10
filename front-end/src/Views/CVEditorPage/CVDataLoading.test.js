42a884a15f0599e9c75c664ddb3b641cb5a0a97c
balasm <01142551@pw.edu.pl>
2020-05-11 00:40:07 +0200

    added test suite for data loading, tests fail :c

--- front-end/src/Views/CVEditorPage/CVDataLoading.test.js
+++ front-end/src/Views/CVEditorPage/CVDataLoading.test.js
@@ -1,10 +1,9 @@
 import React from "react";
-import {render, waitForElement, fireEvent} from '@testing-library/react';
+import {render, waitForElement, fireEvent, queries} from '@testing-library/react';
 import CVEditorPage from "./CVEditorPage";
 import {Router} from "react-router-dom";
 import {createMemoryHistory} from 'history';
 import {UserContext} from "context/UserContext";
-import {getCVdata} from "./functions/other";
 
 const renderWithRouter = (
     ui,
@@ -29,26 +28,27 @@ const renderWithRouter = (
 
 
 describe('load cv data', () => {
-    let apiFail, data, feedback;
+    let apiFail, data, feedback, token;
+    let props = {
+        match: {
+            params: {
+                id: 123
+            }
+        }
+    };
     beforeAll(() => {
-        const getCVData = jest.fn().mockImplementation(() => {
+        token = 123;
+        global.fetch = jest.fn().mockImplementation((input, init) => {
             return new Promise((resolve, reject) => {
                 if (apiFail) {
-                    throw 500;
+                    resolve({status: 500});
+                } else if (data.was_reviewed) {
+                    resolve({status: 200, json: () => {Promise.resolve(data); Promise.resolve(feedback)}});
                 } else {
-                    resolve({status: 200, json: () => Promise.resolve(data)})
+                    resolve({status: 200, json: () => Promise.resolve(data)});
                 }
             })
-        });
-        const getFeedback = jest.fn().mockImplementation(() => {
-            return new Promise((resolve, reject) => {
-                if (apiFail) {
-                    throw 500;
-                } else {
-                    resolve({status: 200, json: () => Promise.resolve(feedback)})
-                }
-            })
-        });
+        })
     });
 
     beforeEach(() => {
@@ -62,7 +62,7 @@ describe('load cv data', () => {
             basic_info: {
                 first_name: "Jan",
                 last_name: "Kowalski",
-                date_of_birth: "01-01-2000",
+                date_of_birth: "01.01.2000",
                 phone_number: '+48123456789',
                 email: "qwe@qwe.qwe"
             },
@@ -92,7 +92,7 @@ describe('load cv data', () => {
         };
         feedback = {
             "basic_info": "dane osobowe ok",
-            "schools": "ddd",
+            "schools": "dd",
             "experiences": "pracy bra",
             "skills": "duzo umiesz",
             "languages": "poliglota",
@@ -101,12 +101,11 @@ describe('load cv data', () => {
     });
 
     it('should load correct data on personal tab', async () => {
-        const {getByLabelText, getByText} = await renderWithRouter(
-            <CVEditorPage />
+        const {getByLabelText} = await renderWithRouter(
+            <CVEditorPage {...props}/>
         );
-
-        expect(getByText("Jan")).toBeInTheDocument();
-        expect(getByLabelText("Imię:").value).toBe(data.basic_info.first_name);
+        await waitForElement(() => fetch);
+        await expect(getByLabelText("Imię:").value).toBe(data.basic_info.first_name);
         expect(getByLabelText("Nazwisko:").value).toBe(data.basic_info.last_name);
         expect(getByLabelText("Email:").value).toBe(data.basic_info.email);
         expect(getByLabelText("Numer telefonu:").value).toBe(data.basic_info.phone_number);
@@ -116,13 +115,59 @@ describe('load cv data', () => {
         const {getByLabelText, getByText} = await renderWithRouter(
             <CVEditorPage />
         );
+
+        const birthDate = getByLabelText("Data urodzenia:").value;
+        expect(birthDate).toEqual(new Date(2000, 0, 0));
+        expect(getByText("01.01.2020")).toBeInTheDocument();
     });
 
     it('should load data and feedback', async () => {
+        data.was_reviewed = true;
+        const {getByLabelText, getByText} = renderWithRouter(
+            <CVEditorPage {...props}/>
+        );
 
+        expect(getByLabelText("Imię:").value).toBe("Jan");
+        expect(getByText("dane osobowe ok")).toBeInTheDocument();
+    });
+
+    it('should render data only', () => {
+        data.was_reviewed = false;
+        const { getByText, getByLabelText } = renderWithRouter(
+            <CVEditorPage />
+        );
+        expect(getByLabelText("Imię:").value).toEqual("Jan");
+        expect(getByText("dane osobowe ok")).not.toBeInTheDocument();
     });
 
     it('should fail on loading data and display alert', async () => {
+        apiFail = true;
+        const { getByText } = renderWithRouter(
+            <CVEditorPage />
+        );
+
+        expect(getByText("Imię:").value).not.toBe("Jan");
+        expect(getByText("Wystąpił błąd", {exact: false})).toBeInTheDocument();
+    });
+
+    it('should change photo placeholder', async () => {
+        data.has_photo = true;
+        const {getByText} = renderWithRouter(
+            <CVEditorPage />
+        );
+
+        await waitForElement(() => fetch);
+        await expect(getByText("Poprzednie zdjęcie")).toBeInTheDocument();
 
     });
+
+    it('should not change photo placeholder', async () => {
+        data.has_photo = false;
+        const {getByText} = renderWithRouter(
+            <CVEditorPage />
+        );
+
+        await waitForElement(() => fetch);
+        await expect(getByText("Wybierz zdjęcie")).toBeInTheDocument();
+    })
 });
