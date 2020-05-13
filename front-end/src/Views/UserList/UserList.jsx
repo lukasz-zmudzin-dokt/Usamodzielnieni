1dfe36cc2a2203abfc74ed5ddac10a8202bffaec
balasm <01142551@pw.edu.pl>
2020-05-13 16:34:17 +0200

    ready component, basic filters added, backend needs fixing

--- front-end/src/Views/UserList/UserList.jsx
+++ front-end/src/Views/UserList/UserList.jsx
@@ -1,19 +1,20 @@
-import React, {useCallback, useContext, useEffect, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {UserContext} from "context/UserContext";
-import qs from 'query-string';
 import proxy from "config/api";
 import {Alert, Card, Container, ListGroup} from "react-bootstrap";
 import UserInfo from "./components/UserInfo";
 import Filters from "./components/Filters";
+import {userTypes} from "constants/userTypes";
+import {userStatuses} from "constants/userStatuses";
 
 const getUsers = async (token, filters) => {
     const {type, status, email, username} = filters;
-    const typeQ = type ? "type=" + type : "";
-    const statusQ = status ? "status=" + status : "";
-    const emailQ = email ? "email=" + email : "";
-    const usernameQ = username ? "username=" + username : "";
+    const typeQ = type ? "&type=" + type : "";
+    const statusQ = status ? "&status=" + status : "";
+    const emailQ = email ? "&email=" + email : "";
+    const usernameQ = username ? "&username=" + username : "";
 
-    const query = `${typeQ}${statusQ}${emailQ}${usernameQ}`;
+    const query = `?${typeQ}${statusQ}${emailQ}${usernameQ}`;
 
     const url = proxy.account + "admin/user_list/all/" + query;
     const headers = {
@@ -40,14 +41,17 @@ const UserList = () => {
     useEffect(() => {
         const loadList = async (token) => {
             setLoading(true);
+            setDisabled(true);
+            setError(false);
             try {
                 const res = await getUsers(token, filters);
                 setUsers(mapUsers(res));
             } catch(e) {
                 console.log(e);
-                setError(true);
+                e !== 404 ? setError(true) : setUsers([]);
             } finally {
                 setLoading(false);
+                setDisabled(false);
             }
         };
         loadList(context.token);
@@ -67,21 +71,21 @@ const UserList = () => {
 
     const mapStatus = status => {
         switch(status) {
-            case "Verified": return "Zweryfikowany";
-            case "Waiting for verification": return "Oczekuje na weryfikację";
-            case "Not verified": return "Odrzucony";
+            case userStatuses.VERIFIED: return "Zweryfikowany";
+            case userStatuses.AWAITING: return "Oczekuje na weryfikację";
+            case userStatuses.REJECTED: return "Odrzucony";
             default: return "Nieznany status";
         }
     };
 
     const mapType = type => {
         switch(type) {
-            case "Staff": return "Administrator";
-            case "Employer": return "Pracodawca";
-            case "Standard": return "Podopieczny";
+            case userTypes.STAFF: return "Administrator";
+            case userTypes.EMPLOYER: return "Pracodawca";
+            case userTypes.STANDARD: return "Podopieczny";
             default: return "Nieznany typ";
         }
-    }
+    };
 
     const msg = error ? (
         <Alert variant="danger">Wystąpił błąd podczas ładowania listy użytkowników.</Alert>
@@ -97,7 +101,13 @@ const UserList = () => {
         <Container>
             <Card>
                 <Card.Header as="h2">Lista użytkowników</Card.Header>
-                {/*<Filters setFilters={setFilters} />*/}
+                <Filters
+                    count={users.length}
+                    setFilter={setFilters}
+                    disabled={disabled}
+                    typeList={mapType}
+                    statusList={mapStatus}
+                />
                 {msg ? (
                     <Card.Body>{msg}</Card.Body>
                 ) : (
---
+++ front-end/src/constants/userStatuses.js
@@ -0,0 +1,5 @@
+export const userStatuses = {
+    VERIFIED: "Verified",
+    REJECTED: "Not verified",
+    AWAITING: "Waiting for verification"
+};
