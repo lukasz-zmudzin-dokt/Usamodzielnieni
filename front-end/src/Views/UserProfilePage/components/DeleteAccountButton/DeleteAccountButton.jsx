eb32208a5041e5b6f013c157d712e2cf118c3961
Damian <7dami77@gmail.com>
2020-05-16 19:12:14 +0200

    structure

---
+++ front-end/src/Views/UserProfilePage/components/DeleteAccountButton/DeleteAccountButton.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+const DeleteAccountButton = () => {
+  return <Button variant="danger">Usuń konto</Button>;
+};
+
+export default DeleteAccountButton;
---
+++ front-end/src/Views/UserProfilePage/components/index.js
@@ -0,0 +1,23 @@
+import { default as AdminApproveUserButton } from "./AdminApproveUserButton/AdminApproveUserButton";
+import { default as AdminOfferApprovalButton } from "./AdminOfferApprovalButton/AdminOfferApprovalButton";
+import { default as AdminRegisterButton } from "./AdminRegisterButton/AdminRegisterButton";
+import { default as CVApprovalButton } from "./CVApprovalButton/CVApprovalButton";
+import { default as DeleteAccountButton } from "./DeleteAccountButton/DeleteAccountButton";
+import { default as EmployerMyOffersButton } from "./EmployerMyOffersButton/EmployerMyOffersButton";
+import { default as UserBasicInfo } from "./UserBasicInfo/UserBasicInfo";
+import { default as UserDetails } from "./UserDetails/UserDetails";
+import { default as UserIcon } from "./UserIcon/UserIcon";
+import { default as UserProperty } from "./UserProperty/UserProperty";
+
+export {
+  AdminApproveUserButton,
+  AdminOfferApprovalButton,
+  AdminRegisterButton,
+  CVApprovalButton,
+  DeleteAccountButton,
+  EmployerMyOffersButton,
+  UserBasicInfo,
+  UserDetails,
+  UserIcon,
+  UserProperty,
+};
