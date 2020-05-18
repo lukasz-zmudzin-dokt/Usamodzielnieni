6a4d91cf94f6eeeb313e502519f559652c6f4cd0
Miko1012 <Miko1012@users.noreply.github.com>
2020-05-18 23:02:45 +0000

    Prettified Code!

--- front-end/src/Views/Footer/Footer.test.jsx
+++ front-end/src/Views/Footer/Footer.test.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import { render, waitForElement } from "@testing-library/react";
 import Footer from "./Footer";
 
-describe('Footer', () => {
-    it('should match snapshot', async () => {
-        const { container, getByText } = render (
-            <Footer />
-        );
-        await waitForElement(() => getByText('instagram'));
-        expect(container).toMatchSnapshot();
-    })
-})
+describe("Footer", () => {
+  it("should match snapshot", async () => {
+    const { container, getByText } = render(<Footer />);
+    await waitForElement(() => getByText("instagram"));
+    expect(container).toMatchSnapshot();
+  });
+});
