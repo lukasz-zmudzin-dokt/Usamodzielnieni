16ed368caa1c581622703d9d4a11ca6a37bb0171
Adrian Mostowski <adaxelx@o2.pl>
2020-05-19 17:16:06 +0200

    some logic changes in edit step

--- front-end/src/Views/Steps/components/EditStep/EditStep.jsx
+++ front-end/src/Views/Steps/components/EditStep/EditStep.jsx
@@ -4,20 +4,24 @@ import { AlertContext, UserContext } from "context";
 import { FormGroup } from "components";
 
 const EditStep = ({ step }) => {
-  const [editedStep, setEditedStep] = useState(step);
+  const [newStep, setNewStep] = useState(step);
   const user = useContext(UserContext);
   const alertC = useRef(useContext(AlertContext));
 
+  const handleSubmit = () => {};
+
   return (
     <Card className="stepInfo">
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Card.Header>
           <FormGroup
             type="text"
             header="Zmień tytuł"
             id="title_change"
-            val={editedStep.title}
-            setVal={(val) => setEditedStep({ ...editedStep, title: val })}
+            required
+            val={newStep.title}
+            setVal={(val) => setNewStep({ ...newStep, title: val })}
+            incorrect="Krok musi mieć tytuł."
           />
         </Card.Header>
         <Card.Body>
@@ -25,29 +29,27 @@ const EditStep = ({ step }) => {
             type="textarea"
             header="Zmień zawartość kroku"
             id="value_change"
-            val={editedStep.value}
-            setVal={(val) => setEditedStep({ ...editedStep, value: val })}
+            required
+            val={newStep.value}
+            setVal={(val) => setNewStep({ ...newStep, value: val })}
           />
-          <FormGroup
-            type="text"
-            header="Zmień zawartość przycisku 1"
-            id="next1_change"
-            val={editedStep.next[0]}
-            setVal={(val) =>
-              setEditedStep({ ...editedStep, next: [val, editedStep.next[1]] })
-            }
-          />
+          {newStep.next.map((val, i) => (
+            <FormGroup
+              type="text"
+              header={`Zmień zawartość przycisku ${i + 1}`}
+              id={`next${i + 1}_change`}
+              required
+              val={newStep.next[i]}
+              setVal={(val) =>
+                setNewStep({
+                  ...newStep,
+                  next: [val, newStep.next[i]],
+                })
+              }
+            />
+          ))}
 
-          <FormGroup
-            type="text"
-            header="Zmień zawartość przycisku 2"
-            val={editedStep.next[1]}
-            id="next2_change"
-            setVal={(val) =>
-              setEditedStep({ ...editedStep, next: [editedStep.next[0], val] })
-            }
-          />
-          <Button>Prześlij zmiany</Button>
+          <Button type="submit">Prześlij zmiany</Button>
         </Card.Body>
       </Form>
     </Card>
