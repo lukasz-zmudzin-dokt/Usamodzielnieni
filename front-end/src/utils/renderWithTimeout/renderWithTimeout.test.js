780acf2d9b9dd6c265583924246675f1bbc0e957
Salabim-mb <Salabim-mb@users.noreply.github.com>
2020-05-20 13:36:03 +0000

    Prettified Code!

--- front-end/src/utils/renderWithTimeout/renderWithTimeout.test.js
+++ front-end/src/utils/renderWithTimeout/renderWithTimeout.test.js
@@ -1,38 +1,36 @@
 import React from "react";
-import {render} from "@testing-library/react";
-import {renderWithTimeout} from "./renderWithTimeout";
+import { render } from "@testing-library/react";
+import { renderWithTimeout } from "./renderWithTimeout";
 
-describe('renderWithTimeout', () => {
-    let component, timeout;
+describe("renderWithTimeout", () => {
+  let component, timeout;
 
-    beforeEach(() => {
-        component = jest.fn().mockImplementation(() => {
-            return <div>środek komponentu</div>
-        });
-        timeout = 10000;
-        jest.useFakeTimers();
-        jest.clearAllMocks();
+  beforeEach(() => {
+    component = jest.fn().mockImplementation(() => {
+      return <div>środek komponentu</div>;
     });
+    timeout = 10000;
+    jest.useFakeTimers();
+    jest.clearAllMocks();
+  });
 
-    it('should render component', () => {
-        const {container} = render(
-            renderWithTimeout(component, timeout)
-        );
+  it("should render component", () => {
+    const { container } = render(renderWithTimeout(component, timeout));
 
-        expect(container).toMatchSnapshot();
-    });
+    expect(container).toMatchSnapshot();
+  });
 
-    it('should hide component after certain time', () => {
-        timeout = 10000;
-        const {getByText, queryByText} = render(
-            renderWithTimeout(component, timeout)
-        );
+  it("should hide component after certain time", () => {
+    timeout = 10000;
+    const { getByText, queryByText } = render(
+      renderWithTimeout(component, timeout)
+    );
 
-        expect(setTimeout).toHaveBeenCalledTimes(1);
-        expect(getByText("środek komponentu")).toBeInTheDocument();
+    expect(setTimeout).toHaveBeenCalledTimes(1);
+    expect(getByText("środek komponentu")).toBeInTheDocument();
 
-        jest.runAllTimers();
+    jest.runAllTimers();
 
-        expect(queryByText("środek komponentu")).not.toBeInTheDocument();
-    });
+    expect(queryByText("środek komponentu")).not.toBeInTheDocument();
+  });
 });
