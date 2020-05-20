e3ee84849b9e9d773b12cac159eb143307391d07
dkacperski97 <dkacperski97@users.noreply.github.com>
2020-05-20 08:30:47 +0000

    Prettified Code!

--- front-end/src/Views/Steps/components/Bullet/Bullet.jsx
+++ front-end/src/Views/Steps/components/Bullet/Bullet.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Bullet = ({ step }) => {
-    return (
-        <>
-            <div className={`bullet bullet--${step.type}`}>
-                {step.type === 'main' && step.title}
-            </div>
-            {step.type === 'sub' && <div className="bullet__title">{step.title}</div>}
-        </>
-    )
-}
+  return (
+    <>
+      <div className={`bullet bullet--${step.type}`}>
+        {step.type === "main" && step.title}
+      </div>
+      {step.type === "sub" && <div className="bullet__title">{step.title}</div>}
+    </>
+  );
+};
 
 export default Bullet;
