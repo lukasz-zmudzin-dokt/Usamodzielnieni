8d08e05383de09e05732dfbe93ff59d859cfa86b
J-Swider <J-Swider@users.noreply.github.com>
2020-05-22 11:39:52 +0000

    Prettified Code!

--- front-end/src/Views/Chats/components/ContactsModalContent/ContactsModalContent.jsx
+++ front-end/src/Views/Chats/components/ContactsModalContent/ContactsModalContent.jsx
@@ -1,75 +1,79 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Container, ListGroup, Alert, Row } from 'react-bootstrap';
-import { UserContext } from 'context';
-import proxy from 'config/api';
-import { Contact } from './Contact';
+import React, { useState, useEffect, useContext } from "react";
+import { Container, ListGroup, Alert, Row } from "react-bootstrap";
+import { UserContext } from "context";
+import proxy from "config/api";
+import { Contact } from "./Contact";
 
 const getContacts = async (token) => {
-    let url = `${proxy.chat}/contacts`; // nie ma endpointu 
-    const headers = {
-        Authorization: "Token " + token,
-        "Content-Type": "application/json"
-    };
+  let url = `${proxy.chat}/contacts`; // nie ma endpointu
+  const headers = {
+    Authorization: "Token " + token,
+    "Content-Type": "application/json",
+  };
 
-    const response = await fetch(url, { method: "GET", headers });
+  const response = await fetch(url, { method: "GET", headers });
 
-    if (response === 200) {
-        return response.json().then(contacts => mapContacts(contacts));
-    } else {
-        throw response.status;
-    }
-}
+  if (response === 200) {
+    return response.json().then((contacts) => mapContacts(contacts));
+  } else {
+    throw response.status;
+  }
+};
 
-const mapContacts = (contacts) => contacts.map(contact => ({
+const mapContacts = (contacts) =>
+  contacts.map((contact) => ({
     id: contact.id,
     first_name: contact.first_name,
     last_name: contact.last_name,
-    role: contact.role
+    role: contact.role,
     // tu nie ma modelu
-}))
+  }));
 
 const ContactsModalContent = () => {
-    const [contacts, setContacts] = useState([]);
-    const [isContactsLoading, setisContactsLoading] = useState(false);
-    const [error, setError] = useState(false);
+  const [contacts, setContacts] = useState([]);
+  const [isContactsLoading, setisContactsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-    const user = useContext(UserContext);
+  const user = useContext(UserContext);
 
-    useEffect(() => {
-        const loadContacts = async (token) => {
-            setisContactsLoading(true);
-            let loadedContacts;
-            try {
-                loadedContacts = await getContacts(token);
-            } catch (e) {
-                loadedContacts = [];
-                setError(true);
-                console.log(e);
-            }
-            setContacts(loadedContacts);
-            setisContactsLoading(false);
-        };
-        loadContacts(user.token);
-    },
-        [user.token]);
+  useEffect(() => {
+    const loadContacts = async (token) => {
+      setisContactsLoading(true);
+      let loadedContacts;
+      try {
+        loadedContacts = await getContacts(token);
+      } catch (e) {
+        loadedContacts = [];
+        setError(true);
+        console.log(e);
+      }
+      setContacts(loadedContacts);
+      setisContactsLoading(false);
+    };
+    loadContacts(user.token);
+  }, [user.token]);
 
-    const msg = error ? <Alert variant="danger">Nie można załadować kontaktów</Alert> :
-        isContactsLoading ? <Alert variant="info">Ładowanie listy kontaktów</Alert> :
-            undefined;
+  const msg = error ? (
+    <Alert variant="danger">Nie można załadować kontaktów</Alert>
+  ) : isContactsLoading ? (
+    <Alert variant="info">Ładowanie listy kontaktów</Alert>
+  ) : undefined;
 
-    return (
-        <Container>
-            {msg ? <Row>{msg}</Row> : (
-                    <ListGroup variant="flush">
-                        {contacts.map((contact) => (
-                            <ListGroup.Item key={contact.id}>
-                                <Contact contact={contact} />
-                            </ListGroup.Item>
-                        ))}
-                    </ListGroup>
-                )}
-        </Container>
-    )
-}
+  return (
+    <Container>
+      {msg ? (
+        <Row>{msg}</Row>
+      ) : (
+        <ListGroup variant="flush">
+          {contacts.map((contact) => (
+            <ListGroup.Item key={contact.id}>
+              <Contact contact={contact} />
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      )}
+    </Container>
+  );
+};
 
-export default ContactsModalContent
+export default ContactsModalContent;
