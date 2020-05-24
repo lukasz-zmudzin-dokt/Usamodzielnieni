b8d019938dec19e8d8ce7675a5b01c55f3df1e10
Miko1012 <mczarkowski7@outlook.com>
2020-05-24 04:21:00 +0200

    tests

--- front-end/src/Views/MyOffersPage/functions/apiCalls.js
+++ front-end/src/Views/MyOffersPage/functions/apiCalls.js
@@ -35,3 +35,33 @@ export const getOfferPeople = async (token, offerId, filters) => {
     throw response.status;
   }
 };
+
+export const setReadStatus = async (token, offerPersonId) => {
+  let url = proxy.job + "employer/application_list/mark-as-read/" + offerPersonId + "/";
+  const headers = {
+    Authorization: "token " + token,
+    "Content-Type": "application/json",
+  };
+
+  const response = await fetch (url, { method: "POST", headers });
+  if(response.status === 200) {
+    return true;
+  } else {
+    throw response.status;
+  }
+};
+
+export const setUnreadStatus = async (token, offerPersonId) => {
+  let url = proxy.job + "employer/application_list/mark-as-unread/" + offerPersonId + "/";
+  const headers = {
+    Authorization: "token " + token,
+    "Content-Type": "application/json",
+  };
+
+  const response = await fetch (url, { method: "POST", headers });
+  if(response.status === 200) {
+    return true;
+  } else {
+    throw response.status;
+  }
+};
