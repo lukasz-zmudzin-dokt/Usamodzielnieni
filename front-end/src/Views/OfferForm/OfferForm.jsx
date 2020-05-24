2a1fa04626d896917ec143ead7bf85a4d4fe8f05
unknown <jakub.swider999@gmail.com>
2020-05-24 20:21:45 +0200

    dealt with a conflict

--- front-end/src/Views/OfferForm/OfferForm.jsx
+++ front-end/src/Views/OfferForm/OfferForm.jsx
@@ -75,10 +75,6 @@ const OfferForm = () => {
       setDisabled(false);
     };
     loadData(context.token);
-<<<<<<< HEAD
-=======
-    console.log(context.data.group_type);
->>>>>>> f38b89800ede37a9e7e62f556408d7b08021a3f6
   }, [
     context.data.company_address,
     context.data.company_name,
