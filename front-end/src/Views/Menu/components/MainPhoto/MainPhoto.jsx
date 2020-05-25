deb980c577d3139631b14226f638bc89b84df9bd
Damian <7dami77@gmail.com>
2020-05-25 03:33:52 +0200

    styles

--- front-end/src/Views/Menu/components/MainPhoto/MainPhoto.jsx
+++ front-end/src/Views/Menu/components/MainPhoto/MainPhoto.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const MainPhoto = () => {
-  return <div></div>;
+  return (
+    <div className="mainPhoto">
+      <svg
+        width="2611.7527"
+        height="1751.2169"
+        viewBox="0 0 2611.7527 1751.2169"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g id="layer2" transform="translate(5.6156163e-7,-140.39752)">
+          <image
+            href="/fot.png"
+            id="image1090"
+            preserveAspectRatio="none"
+            height="1730.6012"
+            width="2569.4835"
+            x="21.134644"
+            y="140.39752"
+          />
+        </g>
+        <g id="layer1" transform="translate(5.6156163e-7,0)">
+          <image href="/main.svg" />
+        </g>
+      </svg>
+    </div>
+  );
 };
 
 export default MainPhoto;
--- front-end/src/Views/Menu/components/TvContainer/TvContainer.jsx
+++ front-end/src/Views/Menu/components/TvContainer/TvContainer.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const TvContainer = () => {
-  return <div></div>;
+  return (
+    <div className="tvContainer">
+      <div className="tvContainer__shapes">
+        <img className="tvContainer__shapes__left" alt="" src="/left_tv.svg" />
+        <img
+          className="tvContainer__shapes__right"
+          alt=""
+          src="/right_tv.svg"
+        />
+      </div>
+      <div className="tvContainer__tv">
+        <img alt="" src="/tv.png" />
+      </div>
+    </div>
+  );
 };
 
 export default TvContainer;
