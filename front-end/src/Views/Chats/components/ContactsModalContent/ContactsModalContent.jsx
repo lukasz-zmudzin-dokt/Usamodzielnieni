3d6348e51760368ff7cf7af0cc6e35f96a55d79e
Adrian Mostowski <adaxelx@o2.pl>
2020-05-26 04:15:26 +0200

    fix pagination

--- front-end/src/Views/Chats/components/ContactsModalContent/ContactsModalContent.jsx
+++ front-end/src/Views/Chats/components/ContactsModalContent/ContactsModalContent.jsx
@@ -3,9 +3,12 @@ import { Container, ListGroup, Alert, Row } from "react-bootstrap";
 import { UserContext } from "context";
 import proxy from "config/api";
 import Contact from "./Contact";
+import { useLocation } from "react-router-dom";
+import qs from "query-string";
+import { Pagination } from "components";
 
-const getContacts = async (token) => {
-  let url = `${proxy.chat}contacts/`;
+const getContacts = async (token, filters) => {
+  let url = `${proxy.chat}contacts/?page=${filters.page}&page_size=${filters.pageSize}`;
   const headers = {
     Authorization: "Token " + token,
     "Content-Type": "application/json",
@@ -15,9 +18,9 @@ const getContacts = async (token) => {
 
   if (response.status === 200) {
     //console.log( await response.json());
-    return response.json().then((contacts) => mapContacts(contacts.results));
+    return response.json();
   } else if (response.status === 2137) {
-    return response.json().then((contacts) => mapContacts(contacts.results));
+    return response.json();
   } else {
     throw response.status;
   }
@@ -35,7 +38,12 @@ const ContactsModalContent = () => {
   const [contacts, setContacts] = useState([]);
   const [isContactsLoading, setisContactsLoading] = useState(false);
   const [error, setError] = useState(false);
-
+  const [count, setCount] = useState(0);
+  const location = useLocation();
+  const [filters, setFilters] = useState({
+    page: 1,
+    pageSize: 5,
+  });
   const user = useContext(UserContext);
 
   useEffect(() => {
@@ -43,23 +51,32 @@ const ContactsModalContent = () => {
       setisContactsLoading(true);
       let loadedContacts;
       try {
-        loadedContacts = await getContacts(token);
+        loadedContacts = await getContacts(token, filters);
       } catch (e) {
         loadedContacts = [];
         setError(true);
         console.log(e);
       }
-      setContacts(loadedContacts);
+      setContacts(mapContacts(loadedContacts.results));
+      setCount(loadedContacts.count);
       setisContactsLoading(false);
     };
     loadContacts(user.token);
-  }, [user.token]);
+  }, [count, filters, user.token]);
 
   const msg = error ? (
     <Alert variant="danger">Nie można załadować kontaktów</Alert>
   ) : isContactsLoading ? (
     <Alert variant="info">Ładowanie listy kontaktów</Alert>
   ) : undefined;
+
+  const queryParams = qs.parse(location.search, { parseNumbers: true });
+  if (
+    typeof queryParams.page === "number" &&
+    queryParams.page !== filters.page
+  ) {
+    setFilters({ ...filters, page: queryParams.page });
+  }
 
   return (
     <Container>
@@ -74,6 +91,10 @@ const ContactsModalContent = () => {
           ))}
         </ListGroup>
       )}
+      <Pagination
+        current={filters.page}
+        max={Math.ceil(count / filters.pageSize)}
+      />
     </Container>
   );
 };
