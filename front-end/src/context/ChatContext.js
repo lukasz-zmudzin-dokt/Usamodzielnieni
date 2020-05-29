daf897690e00ab700d2ef78d2188541e5f8d320c
Adrian Mostowski <adaxelx@o2.pl>
2020-05-29 22:30:07 +0200

    delete useCallback ;)

--- front-end/src/context/ChatContext.js
+++ front-end/src/context/ChatContext.js
@@ -1,10 +1,4 @@
-import React, {
-  createContext,
-  useEffect,
-  useState,
-  useContext,
-  useCallback,
-} from "react";
+import React, { createContext, useEffect, useState, useContext } from "react";
 import proxy from "config/api";
 import { UserContext } from "context";
 
