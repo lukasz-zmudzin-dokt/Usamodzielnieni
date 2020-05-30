3902e86b148946d843cd1ca2316785ddf3be2921
Damian <7dami77@gmail.com>
2020-05-30 18:28:15 +0200

    new notif badge

--- front-end/src/Views/Header/components/NotificationItem.jsx
+++ front-end/src/Views/Header/components/NotificationItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Badge } from "react-bootstrap";
 
 const NotificationItem = ({ notification, ...rest }) => {
   const formatDate = (date) => date.toLocaleDateString(undefined, {});
@@ -7,6 +8,11 @@ const NotificationItem = ({ notification, ...rest }) => {
   return (
     <Link to={notification.path} {...rest}>
       <div>{notification.title}</div>
+      {notification.unread && (
+        <Badge variant="light" className="mr-1">
+          Nowe
+        </Badge>
+      )}
       <small>{formatDate(notification.time)}</small>
     </Link>
   );
