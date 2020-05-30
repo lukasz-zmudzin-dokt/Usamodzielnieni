2a20127426fd3641f40f4d7898532989c10c0fba
balasm <01142551@pw.edu.pl>
2020-05-30 17:56:39 +0200

    damn prettify v2

--- front-end/src/Views/BlogPage/components/Filter/Filter.jsx
+++ front-end/src/Views/BlogPage/components/Filter/Filter.jsx
@@ -8,11 +8,7 @@ import { IndexLinkContainer } from "react-router-bootstrap";
 import { staffTypes } from "constants/staffTypes";
 import { userTypes } from "constants/userTypes";
 import NewVideoBlogModal from "components/NewVideoBlogModal/NewVideoBlogModal";
-<<<<<<< Updated upstream
-import { Redirect, useHistory, useParams } from "react-router-dom";
-=======
-import {useHistory, useParams} from "react-router-dom";
->>>>>>> Stashed changes
+import { useHistory, useParams } from "react-router-dom";
 
 const Filter = ({ setFilter, filtersBlog, count }) => {
   const [filters, setFilters] = useState({ categories: [], tags: [] });
