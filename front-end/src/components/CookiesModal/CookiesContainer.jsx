f48687ef969509ce82989acab08d3c4c3ff93506
Adrian Mostowski <adaxelx@o2.pl>
2020-06-03 00:21:14 +0200

    remove console log

--- front-end/src/components/CookiesModal/CookiesContainer.jsx
+++ front-end/src/components/CookiesModal/CookiesContainer.jsx
@@ -5,7 +5,6 @@ import { UserContext } from "context";
 const CookiesContainer = () => {
   const [show, setShow] = useState(true);
   const user = useContext(UserContext);
-  console.log(user.token);
   return show && !user.token && <CookiesModal hide={() => setShow(false)} />;
 };
 
